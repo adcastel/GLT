b615ef43a0234ac65c8f2f0a3476be43c794111e
Adrian Castello <adcastel@uji.es>
2016-02-17 15:58:19 +0000

    signal functions added

---
+++ test/basic_qt.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <qthread/qthread.h>
+//#include <fast_glt.h>
+//#include <glt.h>
+
+#define N 100
+aligned_t func1(void * arg){
+
+printf("Hola soy el thread %d\n",qthread_shep());
+
+return 0;
+}
+
+int main(int argc, char *argv[]){
+
+//glt_init(argc,argv);
+    qthread_initialize(); //qthreads
+
+//GLT_ult * ults;
+
+//ults = glt_ult_malloc(N);
+aligned_t * ults = (aligned_t *) malloc(sizeof (aligned_t) * N);
+for (int i = 0; i < N; i++){
+       qthread_fork_to((void *) func1, NULL, &ults[i], i%8);
+
+	//glt_ult_creation_to(func1,NULL,&ults[i],i%glt_get_num_threads());
+}
+
+qthread_yield();
+
+for (int i = 0; i < N; i++){
+	//glt_ult_join(&ults[i]);
+    qthread_readFF(NULL, &ults[i]);
+
+}
+//glt_finalize();
+    qthread_finalize();
+
+
+}
