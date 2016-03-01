bb2a5d8b1e7fca98c53563c2b5fe687bf06c1396
Adrian Castello <adcastel@uji.es>
2016-03-01 14:40:27 +0000

    qloop module for qthreads added

--- src/qthreads/fast_glt.h
+++ src/qthreads/fast_glt.h
@@ -21,6 +21,8 @@
 #include <qthread/barrier.h>
 #include <qthread/qtimer.h>
 #include <qthread/sinc.h>
+#include <qthread/qloop.h>
+
 
 
 
@@ -41,6 +43,13 @@
 #define GLT_memory_state aligned_t
 #define GLT_sinc_op qt_sinc_op_f
 #define GLT_sinc qt_sinc_t
+#define GLT_loop_f qt_loop_f
+#define GLT_loopr_f qt_loopr_f
+#define GLT_loop_step_f qt_loop_step_f
+#define GLT_accum_f qt_accum_f
+#define GLT_loop_queue qqloop_handle_t
+#define GLT_loop_queue_kind qt_loop_queue_type
+
 //ARGOBOTS
 #define GLT_event_kind void *
 #define GLT_event_cb_fn void *
@@ -561,6 +570,82 @@ static inline void glt_sinc_wait(GLT_sinc * restrict sinc,  void * restrict targ
     qt_sinc_wait(sinc,target);
 }
 
+static inline int glt_can_loop_functions()
+{
+#ifdef CORE
+    return 0;
+#else
+    return 1;
+#endif
+}
+
+static inline void glt_loop(const size_t start, const size_t end, 
+         const GLT_loop_f func, void * arg){
+    qt_loop(start,end,func,arg);
+}
+
+static inline void glt_loop_step(const size_t start, const size_t end, 
+        const size_t stride, const GLT_loop_step_f func, void * arg){
+    qt_loop_step(start,end,stride,func,arg);
+}
+
+/*static inline void glt_loop_future(const size_t start, const size_t end, 
+         const GLT_loop_f func, void * arg){
+    qt_loop_future(start,end,func,arg);
+}
+
+static inline void glt_loop_step_future(const size_t start, const size_t end, 
+        const size_t stride, const GLT_loop_step_f func, void * arg){
+    qt_loop_step_future(start,end,stride,func,arg);
+}
+*/
+static inline void glt_loop_balance(const size_t start, const size_t end, 
+         const GLT_loop_f func, void * arg){
+    qt_loop_balance(start,end,func,arg);
+}
+
+/*static inline void glt_loop_balance_future(const size_t start, const size_t end, 
+         const GLT_loop_f func, void * arg){
+    qt_loop_balance_future(start,end,func,arg);
+}
+*/
+static inline void glt_loopaccum_balance(const size_t start, const size_t end, 
+         size_t size, void *out, const GLT_loopr_f func, void * arg, GLT_accum_f acc){
+    qt_loopaccum_balance(start,end,size,out,func,arg,acc);
+}
+
+/*static inline void glt_loopaccum_balance_future(const size_t start, const size_t end, 
+size_t size, void *out, const GLT_loop_f func, void * arg, GLT_accum_f acc){
+    qt_loop_balance_future(start,end,size,out,func,arg,acc);
+}
+*/
+
+static inline void glt_loop_queue_create(GLT_loop_queue * loop, GLT_loop_queue_kind kind,
+        const size_t start, const size_t end, 
+        const size_t stride, const GLT_loop_f func, void * arg)
+{
+    loop = qt_loop_queue_create(kind,start,end,stride,func,arg);
+}
+
+static inline void glt_loop_queue_set_chunk(GLT_loop_queue * loop, size_t chunk)
+{
+    qt_loop_queue_setchunk(loop, chunk);
+}
+
+static inline void glt_loop_queue_run(GLT_loop_queue * loop)
+{
+    qt_loop_queue_run(loop);
+}
+
+static inline void glt_loop_queue_run_there(GLT_loop_queue * loop, GLT_thread id)
+{
+    qt_loop_queue_run_there(loop,id);
+}
+
+static inline void glt_loop_queue_add_thread(GLT_loop_queue * loop, GLT_thread id)
+{
+    qt_loop_queue_addworker(loop,id);
+}
 //ARGOBOTS FUNCTIONS that are not supported by Qthreads
 
 static inline int glt_can_event_functions()
