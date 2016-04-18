a1f44e548975aa80f602b8429bc644aeb6afa36b
Adrian Castello <adcastel@uji.es>
2016-04-18 14:24:47 +0000

    qthreads glt_timer.c file updated

--- src/qthreads/glt_timer.c
+++ src/qthreads/glt_timer.c
@@ -1,34 +1,40 @@
+#ifndef FASTGLT
 #include <glt.h>
+#define GLT_return 
+#else
+#define GLT_return static inline
+#endif
+
 #include <sys/time.h>
 
-double glt_get_wtime() 
+GLT_return double glt_get_wtime() 
 {
     struct timeval time;
     gettimeofday(&time,NULL);
     return (time.tv_sec * 1000000 + time.tv_usec)/1000000.0;
 }
 
-void glt_timer_create(GLT_timer * timer) 
+GLT_return void glt_timer_create(GLT_timer * timer) 
 {
     *timer = qtimer_create();
 }
 
-void glt_timer_free(GLT_timer * timer) 
+GLT_return void glt_timer_free(GLT_timer * timer) 
 {
     qtimer_destroy(*timer);
 }
 
-void glt_timer_start(GLT_timer timer) 
+GLT_return void glt_timer_start(GLT_timer timer) 
 {
     qtimer_start(timer);
 }
 
-void glt_timer_stop(GLT_timer timer) 
+GLT_return void glt_timer_stop(GLT_timer timer) 
 {
     qtimer_stop(timer);
 }
 
-void glt_timer_get_secs(GLT_timer timer, double *secs) 
+GLT_return void glt_timer_get_secs(GLT_timer timer, double *secs) 
 {
     *secs = qtimer_secs(timer);
 }
