8afd007b3fdf536e28a9c5ec2c72007c2d7477a3
Adrian Castello <adcastel@uji.es>
2016-04-18 14:24:47 +0000

    qthreads glt_sinc.c file updated

--- src/qthreads/glt_sinc.c
+++ src/qthreads/glt_sinc.c
@@ -4,9 +4,14 @@
  * and open the template in the editor.
  */
 
+#ifndef FASTGLT
 #include <glt.h>
+#define GLT_return 
+#else
+#define GLT_return static inline
+#endif
 
- int glt_can_sinc_functions()
+GLT_return int glt_can_sinc_functions()
 {
 #ifdef CORE
     return 0;
@@ -17,39 +22,39 @@
 
 #ifndef CORE
 
- void glt_sinc_create(GLT_sinc *sinc, size_t sizeof_value,
+GLT_return void glt_sinc_create(GLT_sinc *sinc, size_t sizeof_value,
         const void * initial_value, GLT_sinc_op op, size_t expected)
 {
     sinc = qt_sinc_create(sizeof_value, initial_value, op, expected);
 }
 
- void glt_sinc_init(GLT_sinc *restrict sinc, size_t sizeof_value,
+GLT_return void glt_sinc_init(GLT_sinc *restrict sinc, size_t sizeof_value,
         const void * initial_value, GLT_sinc_op op, size_t expected)
 {
     qt_sinc_init(sinc, sizeof_value, initial_value, op, expected);
 }
 
- void glt_sinc_destroy(GLT_sinc *sinc)
+GLT_return void glt_sinc_destroy(GLT_sinc *sinc)
 {
     qt_sinc_destroy(sinc);
 }
 
- void glt_sinc_fini(GLT_sinc * sinc)
+GLT_return void glt_sinc_fini(GLT_sinc * sinc)
 {
     qt_sinc_fini(sinc);
 }
 
- void glt_sinc_reset(GLT_sinc * sinc, size_t expect)
+GLT_return void glt_sinc_reset(GLT_sinc * sinc, size_t expect)
 {
     qt_sinc_reset(sinc,expect);
 }
 
- void glt_sinc_submit(GLT_sinc * restrict sinc, const void * restrict value)
+GLT_return void glt_sinc_submit(GLT_sinc * restrict sinc, const void * restrict value)
 {
     qt_sinc_submit(sinc,value);
 }
 
- void glt_sinc_wait(GLT_sinc * restrict sinc,  void * restrict target)
+GLT_return void glt_sinc_wait(GLT_sinc * restrict sinc,  void * restrict target)
 {
     qt_sinc_wait(sinc,target);
 }
