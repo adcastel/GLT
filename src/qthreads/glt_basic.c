42955778c2ba2c074838e0a0fde397cf44dd641c
Adrian Castello <adcastel@uji.es>
2016-04-18 14:24:45 +0000

    qthreads glt_basic.c file updated

--- src/qthreads/glt_basic.c
+++ src/qthreads/glt_basic.c
@@ -4,9 +4,14 @@
  * and open the template in the editor.
  */
 
+#ifndef FASTGLT
 #include <glt.h>
+#define GLT_return 
+#else
+#define GLT_return static inline
+#endif
 
- int glt_can_extended_basic()
+GLT_return int glt_can_extended_basic()
 {
 #ifdef CORE
     return 0;
@@ -18,53 +23,49 @@
 #ifndef CORE
 
 
-/* void glt_ult_creation_precond(void(*thread_func)(void *), void * arg,
-        GLT_ult * ult, int npreconds, ...){
-    qthread_fork_precond((void *)thread_func,arg,ult,npreconds,...);
-}
+//void glt_ult_creation_precond(void(*thread_func)(void *), void * arg,
+//        GLT_ult * ult, int npreconds, ...){
+//    qthread_fork_precond((void *)thread_func,arg,ult,npreconds,...);
+//}
+//
+// void glt_ult_creation_precond_to(void(*thread_func)(void *), void * arg,
+//        GLT_ult * ult, int dest,int npreconds, ...){
+//    qthread_fork_precond_to((void *)thread_func,arg,ult,dest,npreconds,...);
+//}
 
- void glt_ult_creation_precond_to(void(*thread_func)(void *), void * arg,
-        GLT_ult * ult, int dest,int npreconds, ...){
-    qthread_fork_precond_to((void *)thread_func,arg,ult,dest,npreconds,...);
-}
-*/
- void glt_ult_creation_syncvar(void(*thread_func)(void *), void * arg,
+GLT_return void glt_ult_creation_syncvar(void(*thread_func)(void *), void * arg,
         GLT_syncvar *syncvar){
     qthread_fork_syncvar((void *)thread_func,arg,syncvar);
 }
 
- void glt_ult_creation_syncvar_to(void(*thread_func)(void *), void * arg,
+GLT_return void glt_ult_creation_syncvar_to(void(*thread_func)(void *), void * arg,
         GLT_syncvar *syncvar, int dest,int npreconds, ...){
     qthread_fork_syncvar_to((void *)thread_func,arg,syncvar,dest);
 }
 
- void glt_ult_get_thread(GLT_thread *thread, int *worker)
+GLT_return void glt_ult_get_thread(GLT_thread *thread, int *worker)
 {
     *worker = qthread_worker(thread);
 }
 
- void glt_ult_get_thread_unique(GLT_thread *thread, int *worker)
+GLT_return void glt_ult_get_thread_unique(GLT_thread *thread, int *worker)
 {
     *worker = qthread_worker_unique(thread);
 }
 
- void glt_ult_get_tasklocal(void * pointer, unsigned int size)
+GLT_return void glt_ult_get_tasklocal(void * pointer, unsigned int size)
 {
     pointer = qthread_get_tasklocal(size);
 }
 
- void glt_ult_size_tasklocal(unsigned int *size)
+GLT_return void glt_ult_size_tasklocal(unsigned int *size)
 {
     *size = qthread_size_tasklocal();
 }
 
-
-
- void glt_ult_get_stack_left(size_t *size)
+GLT_return void glt_ult_get_stack_left(size_t *size)
 {
     *size = qthread_stackleft();
 }
-
-
 
 #endif
