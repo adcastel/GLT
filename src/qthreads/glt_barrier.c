78258eb486084a93efa62aa96661bb3a71dc082e
Adrian Castello <adcastel@uji.es>
2016-04-18 14:24:45 +0000

    qthreads glt_barrier.c file updated

--- src/qthreads/glt_barrier.c
+++ src/qthreads/glt_barrier.c
@@ -1,17 +1,20 @@
+#ifndef FASTGLT
 #include <glt.h>
-
-void glt_barrier_create(int num_waiters, GLT_barrier *barrier)
+#define GLT_return 
+#else
+#define GLT_return static inline
+#endif
+GLT_return void glt_barrier_create(int num_waiters, GLT_barrier *barrier)
 {
     barrier = qt_barrier_create(num_waiters, UPLOCK);
 }
 
-void glt_barrier_free(GLT_barrier *barrier)
+GLT_return void glt_barrier_free(GLT_barrier *barrier)
 {
     qt_barrier_destroy(barrier);
 }
 
-
-void glt_barrier_wait(GLT_barrier *barrier)
+GLT_return void glt_barrier_wait(GLT_barrier *barrier)
 {
     qt_barrier_enter(barrier);
 }
