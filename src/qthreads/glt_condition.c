e993eeb312f1648c9b6a075dc9305119e6be3d1f
Adrian Castello <adcastel@uji.es>
2016-04-18 14:24:46 +0000

    qthreads glt_condition.c file updated

--- src/qthreads/glt_condition.c
+++ src/qthreads/glt_condition.c
@@ -1,21 +1,26 @@
+#ifndef FASTGLT
 #include <glt.h>
+#define GLT_return 
+#else
+#define GLT_return static inline
+#endif
 
-void glt_cond_create(GLT_cond *cond)
+GLT_return void glt_cond_create(GLT_cond *cond)
 {
     cond = (GLT_cond *)malloc(sizeof(GLT_cond));
 }
 
-void glt_cond_free(GLT_cond *cond)
+GLT_return void glt_cond_free(GLT_cond *cond)
 {
     free(cond);
 }
 
-void glt_cond_signal(GLT_cond cond)
+GLT_return void glt_cond_signal(GLT_cond cond)
 {
     qthread_empty(&cond);
 }
 
-void glt_cond_wait(GLT_cond cond, GLT_mutex mutex)
+GLT_return void glt_cond_wait(GLT_cond cond, GLT_mutex mutex)
 {
     /*Waits for memory to become empty and then fills it*/
     aligned_t fill=1;
@@ -23,7 +28,7 @@ void glt_cond_wait(GLT_cond cond, GLT_mutex mutex)
     qthread_lock(mutex);
 }
 
-void glt_cond_broadcast(GLT_cond cond)
+GLT_return void glt_cond_broadcast(GLT_cond cond)
 {
     qthread_empty(&cond);
 }
