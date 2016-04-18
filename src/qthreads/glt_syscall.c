5f9a53ffbaef0495dfc2f6cc2ca166bc77cf17b5
Adrian Castello <adcastel@uji.es>
2016-04-18 14:24:47 +0000

    qthreads glt_syscall.c file updated

--- src/qthreads/glt_syscall.c
+++ src/qthreads/glt_syscall.c
@@ -4,9 +4,14 @@
  * and open the template in the editor.
  */
 
+#ifndef FASTGLT
 #include <glt.h>
+#define GLT_return 
+#else
+#define GLT_return static inline
+#endif
 
- int glt_can_syscall_functions()
+GLT_return int glt_can_syscall_functions()
 {
 #ifdef CORE
     return 0;
@@ -17,68 +22,68 @@
 
 #ifndef CORE
 
- void glt_syscall_begin_blocking()
+GLT_return void glt_syscall_begin_blocking()
 {
      qt_begin_blocking_action(); 
 }
 
- void glt_syscall_end_blocking()
+GLT_return void glt_syscall_end_blocking()
 {
      qt_end_blocking_action(); 
 }
 
- int glt_syscall_accept(int socket, 
+GLT_return int glt_syscall_accept(int socket, 
         struct sockaddr *restrict address, socklen_t *restrict address_len)
 {
     return qt_accept (socket, address, address_len);
 }
 
- int glt_syscall_connect(int socket, 
+GLT_return int glt_syscall_connect(int socket, 
         struct sockaddr *restrict address, socklen_t address_len)
 {
     return qt_connect (socket, address, address_len);
 }
 
- int glt_syscall_poll(struct pollfd fds[], nfds_t nfds, int timeout)
+GLT_return int glt_syscall_poll(struct pollfd fds[], nfds_t nfds, int timeout)
 {
     return qt_poll (fds, nfds, timeout);
 }
 
- size_t glt_syscall_pread(int filedes, void *buf, size_t nbyte, 
+GLT_return size_t glt_syscall_pread(int filedes, void *buf, size_t nbyte, 
         off_t offset)
 {
     return qt_pread (filedes, buf, nbyte, offset);
 }
 
- size_t glt_syscall_read(int filedes, void *buf, size_t nbyte)
+GLT_return size_t glt_syscall_read(int filedes, void *buf, size_t nbyte)
 {
     return qt_read (filedes, buf, nbyte);
 }
 
- size_t glt_syscall_pwrite(int filedes, void *buf, size_t nbyte, 
+GLT_return size_t glt_syscall_pwrite(int filedes, void *buf, size_t nbyte, 
         off_t offset)
 {
     return qt_pwrite (filedes, buf, nbyte, offset);
 }
 
- size_t glt_syscall_write(int filedes, void *buf, size_t nbyte)
+GLT_return size_t glt_syscall_write(int filedes, void *buf, size_t nbyte)
 {
     return qt_write (filedes, buf, nbyte);
 }
 
- int glt_syscall_select(int nfds, fd_set *restrict readfds, 
+GLT_return int glt_syscall_select(int nfds, fd_set *restrict readfds, 
         fd_set *restrict writefds, fd_set *restrict errorfds, 
         struct timeval *restrict timeout)
 {
     return qt_select (nfds, readfds, writefds, errorfds, timeout);
 }
 
- int glt_syscall_system(const char * command)
+GLT_return int glt_syscall_system(const char * command)
 {
     return qt_system(command);
 }
 
- int glt_syscall_wait4 (pid_t pid, int *stat_loc, int options, 
+GLT_return int glt_syscall_wait4 (pid_t pid, int *stat_loc, int options, 
         struct rusage *rusage)
 {
     return qt_wait4 ( pid, stat_loc, options, rusage);
