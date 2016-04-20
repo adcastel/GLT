aca146d04ec0a1cd47882bb70eafa10ff0f3a7c9
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:56 +0000

    qthreadss glt_condition.c file updated with new prefix

--- src/qthreads/glt_condition.c
+++ src/qthreads/glt_condition.c
@@ -1,26 +1,26 @@
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
-GLT_return void glt_cond_create(GLT_cond *cond)
+GLT_func_prefix void glt_cond_create(GLT_cond *cond)
 {
     cond = (GLT_cond *)malloc(sizeof(GLT_cond));
 }
 
-GLT_return void glt_cond_free(GLT_cond *cond)
+GLT_func_prefix void glt_cond_free(GLT_cond *cond)
 {
     free(cond);
 }
 
-GLT_return void glt_cond_signal(GLT_cond cond)
+GLT_func_prefix void glt_cond_signal(GLT_cond cond)
 {
     qthread_empty(&cond);
 }
 
-GLT_return void glt_cond_wait(GLT_cond cond, GLT_mutex mutex)
+GLT_func_prefix void glt_cond_wait(GLT_cond cond, GLT_mutex mutex)
 {
     /*Waits for memory to become empty and then fills it*/
     aligned_t fill=1;
@@ -28,7 +28,7 @@ GLT_return void glt_cond_wait(GLT_cond cond, GLT_mutex mutex)
     qthread_lock(mutex);
 }
 
-GLT_return void glt_cond_broadcast(GLT_cond cond)
+GLT_func_prefix void glt_cond_broadcast(GLT_cond cond)
 {
     qthread_empty(&cond);
 }
