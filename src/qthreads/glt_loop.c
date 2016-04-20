3fbe8584982eaf5008c0182f7b0c52acf9e3624c
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:57 +0000

    qthreadss glt_loop.c file updated with new prefix

--- src/qthreads/glt_loop.c
+++ src/qthreads/glt_loop.c
@@ -6,12 +6,12 @@
 
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
-GLT_return int glt_can_loop_functions()
+GLT_func_prefix int glt_can_loop_functions()
 {
 #ifdef CORE
     return 0;
@@ -22,74 +22,74 @@ GLT_return int glt_can_loop_functions()
 
 #ifndef CORE
 
-GLT_return void glt_loop(const size_t start, const size_t end, 
+GLT_func_prefix void glt_loop(const size_t start, const size_t end, 
          const GLT_loop_f func, void * arg){
     qt_loop(start,end,func,arg);
 }
 
 
  //This function is found in the header but not in the static or dynamic qthreads libraries
-//GLT_returnvoid glt_loop_step(const size_t start, const size_t end, 
+//GLT_func_prefixvoid glt_loop_step(const size_t start, const size_t end, 
 //        const size_t stride, const GLT_loop_step_f func, void * arg)
 //{
 //    GLT_LIB_ERROR;
     //qt_loop_step(start,end,stride,func,arg);
 //}
 
-/*GLT_return void glt_loop_future(const size_t start, const size_t end, 
+/*GLT_func_prefix void glt_loop_future(const size_t start, const size_t end, 
          const GLT_loop_f func, void * arg){
     qt_loop_future(start,end,func,arg);
 }
 
-GLT_return void glt_loop_step_future(const size_t start, const size_t end, 
+GLT_func_prefix void glt_loop_step_future(const size_t start, const size_t end, 
         const size_t stride, const GLT_loop_step_f func, void * arg){
     qt_loop_step_future(start,end,stride,func,arg);
 }
 */
-GLT_return void glt_loop_balance(const size_t start, const size_t end, 
+GLT_func_prefix void glt_loop_balance(const size_t start, const size_t end, 
          const GLT_loop_f func, void * arg){
     qt_loop_balance(start,end,func,arg);
 }
 
-/* GLT_return void glt_loop_balance_future(const size_t start, const size_t end, 
+/* GLT_func_prefix void glt_loop_balance_future(const size_t start, const size_t end, 
          const GLT_loop_f func, void * arg){
     qt_loop_balance_future(start,end,func,arg);
 }
 */
-GLT_return void glt_loopaccum_balance(const size_t start, const size_t end, 
+GLT_func_prefix void glt_loopaccum_balance(const size_t start, const size_t end, 
          size_t size, void *out, const GLT_loopr_f func, void * arg, GLT_accum_f acc){
     qt_loopaccum_balance(start,end,size,out,func,arg,acc);
 }
 
-/*GLT_return void glt_loopaccum_balance_future(const size_t start, const size_t end, 
+/*GLT_func_prefix void glt_loopaccum_balance_future(const size_t start, const size_t end, 
 size_t size, void *out, const GLT_loop_f func, void * arg, GLT_accum_f acc){
     qt_loop_balance_future(start,end,size,out,func,arg,acc);
 }
 */
 
-GLT_return void glt_loop_queue_create(GLT_loop_queue * loop, GLT_loop_queue_kind kind,
+GLT_func_prefix void glt_loop_queue_create(GLT_loop_queue * loop, GLT_loop_queue_kind kind,
         const size_t start, const size_t end, 
         const size_t stride, const GLT_loop_f func, void * arg)
 {
     loop = qt_loop_queue_create(kind,start,end,stride,func,arg);
 }
 
-GLT_return void glt_loop_queue_set_chunk(GLT_loop_queue * loop, size_t chunk)
+GLT_func_prefix void glt_loop_queue_set_chunk(GLT_loop_queue * loop, size_t chunk)
 {
     qt_loop_queue_setchunk(loop, chunk);
 }
 
-GLT_return void glt_loop_queue_run(GLT_loop_queue * loop)
+GLT_func_prefix void glt_loop_queue_run(GLT_loop_queue * loop)
 {
     qt_loop_queue_run(loop);
 }
 
-GLT_return void glt_loop_queue_run_there(GLT_loop_queue * loop, GLT_thread id)
+GLT_func_prefix void glt_loop_queue_run_there(GLT_loop_queue * loop, GLT_thread id)
 {
     qt_loop_queue_run_there(loop,id);
 }
 
-GLT_return void glt_loop_queue_add_thread(GLT_loop_queue * loop, GLT_thread id)
+GLT_func_prefix void glt_loop_queue_add_thread(GLT_loop_queue * loop, GLT_thread id)
 {
     qt_loop_queue_addworker(loop,id);
 }
