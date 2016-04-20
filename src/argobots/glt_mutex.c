3543ff645b7c528953661982a01a2b2ff7a17222
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:43 +0000

    argobots glt_mutex.c file updated with new prefix

--- src/argobots/glt_mutex.c
+++ src/argobots/glt_mutex.c
@@ -1,40 +1,40 @@
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
 
 
 
-GLT_return void glt_mutex_create(GLT_mutex * mutex)
+GLT_func_prefix void glt_mutex_create(GLT_mutex * mutex)
 {
     ABT_mutex_create(mutex);
 }
 
-GLT_return void glt_mutex_lock(GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_lock(GLT_mutex mutex)
 {
     ABT_mutex_lock(mutex);
 }
 
-GLT_return void glt_mutex_unlock(GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_unlock(GLT_mutex mutex)
 {
     ABT_mutex_unlock(mutex);
 }
 
-GLT_return void glt_mutex_free(GLT_mutex * mutex)
+GLT_func_prefix void glt_mutex_free(GLT_mutex * mutex)
 {
     ABT_mutex_free(mutex);
 }
 
-GLT_return void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex)
 {
     *locked = ABT_mutex_trylock (mutex);
 }
 
 //extended mutex functions
-GLT_return int glt_can_extended_mutex()
+GLT_func_prefix int glt_can_extended_mutex()
 {
 #ifdef CORE
     return 0;
@@ -45,22 +45,22 @@ GLT_return int glt_can_extended_mutex()
 
 #ifndef CORE
 
-GLT_return void glt_mutex_lock_low(GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_lock_low(GLT_mutex mutex)
 {
    // ABT_mutex_lock_low (mutex);
 } 
 
-GLT_return void glt_mutex_spinlock (GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_spinlock (GLT_mutex mutex)
 {
     ABT_mutex_spinlock (mutex);
 }
 
-GLT_return void glt_mutex_unlock_se (GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_unlock_se (GLT_mutex mutex)
 {
    // ABT_mutex_unlock_se (mutex);
 }
 
-GLT_return void glt_mutex_equal (GLT_mutex mutex1, GLT_mutex mutex2, GLT_bool *result)
+GLT_func_prefix void glt_mutex_equal (GLT_mutex mutex1, GLT_mutex mutex2, GLT_bool *result)
 {
     ABT_mutex_equal (mutex1, mutex2, result);
 }
