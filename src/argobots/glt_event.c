30b2eba5e9932e8f799097bec35def8e16702b99
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:43 +0000

    argobots glt_event.c file updated with new prefix

--- src/argobots/glt_event.c
+++ src/argobots/glt_event.c
@@ -6,13 +6,13 @@
 
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
 //Event functions
-GLT_return int glt_can_event_functions()
+GLT_func_prefix int glt_can_event_functions()
 {
 #ifdef CORE
     return 0;
@@ -22,12 +22,12 @@ GLT_return int glt_can_event_functions()
 }
 
 #ifndef CORE
-GLT_return void glt_get_error_str(int errno, char *str, size_t *len)
+GLT_func_prefix void glt_get_error_str(int errno, char *str, size_t *len)
 {
     ABT_error_get_str (errno,str,len);
 }
 
-GLT_return void glt_event_add_callbac (GLT_event_kind event, GLT_event_cb_fn ask_cb,
+GLT_func_prefix void glt_event_add_callbac (GLT_event_kind event, GLT_event_cb_fn ask_cb,
         void *ask_user_arg, GLT_event_cb_fn act_cb, void *act_user_arg, 
         int *cb_id)
 {
@@ -35,7 +35,7 @@ GLT_return void glt_event_add_callbac (GLT_event_kind event, GLT_event_cb_fn ask
             act_user_arg, cb_id);
 }
 
-GLT_return void glt_event_del_callback (GLT_event_kind event, int cb_id)
+GLT_func_prefix void glt_event_del_callback (GLT_event_kind event, int cb_id)
 {
     ABT_event_del_callback (event, cb_id);
 }
