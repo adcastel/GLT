50e12d70b2162c15602df46276d96099f97f118f
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:57 +0000

    qthreadss glt_runtime.c file updated with new prefix

--- src/qthreads/glt_runtime.c
+++ src/qthreads/glt_runtime.c
@@ -6,12 +6,12 @@
 
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
-GLT_return int glt_can_extended_runtime()
+GLT_func_prefix int glt_can_extended_runtime()
 {
 #ifdef CORE
     return 0;
@@ -22,62 +22,62 @@ GLT_return int glt_can_extended_runtime()
 
 #ifndef CORE
 
-GLT_return void glt_subthread_get_num(GLT_subthread *num)
+GLT_func_prefix void glt_subthread_get_num(GLT_subthread *num)
 {
     *num = qthread_num_workers();
 }
 
-GLT_return void glt_cacheline(int *cl)
+GLT_func_prefix void glt_cacheline(int *cl)
 {
     *cl = qthread_cacheline();
 }
 
-GLT_return void glt_sorted_threads(const GLT_thread * list)
+GLT_func_prefix void glt_sorted_threads(const GLT_thread * list)
 {
     list = qthread_sorted_sheps();
 }
 
-GLT_return void glt_sorted_threads_remote(const GLT_thread * list, const GLT_thread src)
+GLT_func_prefix void glt_sorted_threads_remote(const GLT_thread * list, const GLT_thread src)
 {
     list = qthread_sorted_sheps_remote(src);
 }
 
-GLT_return void glt_debuglevel(size_t *level, const enum introspective_state type)
+GLT_func_prefix void glt_debuglevel(size_t *level, const enum introspective_state type)
 {
     *level = qthread_debuglevel(type);
 }
 
-GLT_return void glt_readstate(size_t *value, const enum introspective_state type)
+GLT_func_prefix void glt_readstate(size_t *value, const enum introspective_state type)
 {
     *value = qthread_readstate(type);
 }
 
-GLT_return void glt_thread_distance(int * dist, const GLT_thread src, const GLT_thread dst)
+GLT_func_prefix void glt_thread_distance(int * dist, const GLT_thread src, const GLT_thread dst)
 {
     *dist = qthread_distance(src,dst);
 }
 
-GLT_return void glt_thread_enable(GLT_thread id)
+GLT_func_prefix void glt_thread_enable(GLT_thread id)
 {
     qthread_enable_shepherd(id);
 }
 
-GLT_return void glt_thread_disable(GLT_bool *res, GLT_thread id)
+GLT_func_prefix void glt_thread_disable(GLT_bool *res, GLT_thread id)
 {
     *res = qthread_disable_shepherd(id);
 }
 
-GLT_return void glt_subthread_enable(GLT_subthread id)
+GLT_func_prefix void glt_subthread_enable(GLT_subthread id)
 {
     qthread_enable_worker(id);
 }
 
-GLT_return void glt_subthread_disable(GLT_bool *res, GLT_subthread id)
+GLT_func_prefix void glt_subthread_disable(GLT_bool *res, GLT_subthread id)
 {
     *res = qthread_disable_worker(id);
 }
 
-GLT_return void glt_thread_ok(GLT_bool *res)
+GLT_func_prefix void glt_thread_ok(GLT_bool *res)
 {
     *res = qthread_shep_ok();
 }
