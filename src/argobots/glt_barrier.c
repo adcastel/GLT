344c3e30b8adb8cbe5e211b3f571599a2f927fd9
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:42 +0000

    argobots glt_barrier.c file updated with new prefix

--- src/argobots/glt_barrier.c
+++ src/argobots/glt_barrier.c
@@ -1,21 +1,21 @@
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
-GLT_return void glt_barrier_create(int num_waiters, GLT_barrier *barrier)
+GLT_func_prefix void glt_barrier_create(int num_waiters, GLT_barrier *barrier)
 {
     ABT_barrier_create(num_waiters, barrier);
 }
 
-GLT_return void glt_barrier_free(GLT_barrier *barrier)
+GLT_func_prefix void glt_barrier_free(GLT_barrier *barrier)
 {
     ABT_barrier_free(barrier);
 }
 
-GLT_return void glt_barrier_wait(GLT_barrier *barrier)
+GLT_func_prefix void glt_barrier_wait(GLT_barrier *barrier)
 {
     ABT_barrier_wait(*barrier);
 }
