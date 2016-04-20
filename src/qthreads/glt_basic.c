a35d2888dc4eaab4c48c0ba54e8deb1e76b7738e
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:56 +0000

    qthreadss glt_basic.c file updated with new prefix

--- src/qthreads/glt_basic.c
+++ src/qthreads/glt_basic.c
@@ -6,12 +6,12 @@
 
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
-GLT_return int glt_can_extended_basic()
+GLT_func_prefix int glt_can_extended_basic()
 {
 #ifdef CORE
     return 0;
@@ -33,37 +33,37 @@ GLT_return int glt_can_extended_basic()
 //    qthread_fork_precond_to((void *)thread_func,arg,ult,dest,npreconds,...);
 //}
 
-GLT_return void glt_ult_creation_syncvar(void(*thread_func)(void *), void * arg,
+GLT_func_prefix void glt_ult_creation_syncvar(void(*thread_func)(void *), void * arg,
         GLT_syncvar *syncvar){
     qthread_fork_syncvar((void *)thread_func,arg,syncvar);
 }
 
-GLT_return void glt_ult_creation_syncvar_to(void(*thread_func)(void *), void * arg,
+GLT_func_prefix void glt_ult_creation_syncvar_to(void(*thread_func)(void *), void * arg,
         GLT_syncvar *syncvar, int dest,int npreconds, ...){
     qthread_fork_syncvar_to((void *)thread_func,arg,syncvar,dest);
 }
 
-GLT_return void glt_ult_get_thread(GLT_thread *thread, int *worker)
+GLT_func_prefix void glt_ult_get_thread(GLT_thread *thread, int *worker)
 {
     *worker = qthread_worker(thread);
 }
 
-GLT_return void glt_ult_get_thread_unique(GLT_thread *thread, int *worker)
+GLT_func_prefix void glt_ult_get_thread_unique(GLT_thread *thread, int *worker)
 {
     *worker = qthread_worker_unique(thread);
 }
 
-GLT_return void glt_ult_get_tasklocal(void * pointer, unsigned int size)
+GLT_func_prefix void glt_ult_get_tasklocal(void * pointer, unsigned int size)
 {
     pointer = qthread_get_tasklocal(size);
 }
 
-GLT_return void glt_ult_size_tasklocal(unsigned int *size)
+GLT_func_prefix void glt_ult_size_tasklocal(unsigned int *size)
 {
     *size = qthread_size_tasklocal();
 }
 
-GLT_return void glt_ult_get_stack_left(size_t *size)
+GLT_func_prefix void glt_ult_get_stack_left(size_t *size)
 {
     *size = qthread_stackleft();
 }
