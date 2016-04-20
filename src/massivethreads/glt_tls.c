512c374e20341409540c8d084ec0e9033fec8630
Adrian Castello <adcastel@uji.es>
2016-04-20 13:07:02 +0000

    massivethreads glt_tls.c file updated with new prefix

--- src/massivethreads/glt_tls.c
+++ src/massivethreads/glt_tls.c
@@ -4,9 +4,13 @@
  * and open the template in the editor.
  */
 
+#ifndef FASTGLT
 #include <glt.h>
-
- int glt_can_tls_functions()
+#define GLT_func_prefix 
+#else
+#define GLT_func_prefix static inline
+#endif
+GLT_func_prefix int glt_can_tls_functions()
 {
 #ifdef CORE
     return 0;
@@ -17,22 +21,22 @@
 
 #ifndef CORE
 
- void glt_key_create (void(*destructor)(void *value), GLT_key *newkey)
+GLT_func_prefix void glt_key_create (void(*destructor)(void *value), GLT_key *newkey)
 {
     myth_key_create(newkey,destructor);
 }
 
- void glt_key_free (GLT_key *key)
+GLT_func_prefix void glt_key_free (GLT_key *key)
 {
     myth_key_delete (*key);
 }
 
- void glt_key_set (GLT_key key, void *value)
+GLT_func_prefix void glt_key_set (GLT_key key, void *value)
 {
     myth_setspecific (key,value);
 }
 
- void glt_key_get (GLT_key key, void **value)
+GLT_func_prefix void glt_key_get (GLT_key key, void **value)
 {
     *value = myth_getspecific (key);
 }
