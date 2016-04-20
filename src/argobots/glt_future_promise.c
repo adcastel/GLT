519d47a3c335bac97457e9f5e08afdcd6ca41046
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:43 +0000

    argobots glt_future_promise.c file updated with new prefix

--- src/argobots/glt_future_promise.c
+++ src/argobots/glt_future_promise.c
@@ -6,13 +6,13 @@
 
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
 //future functions
-GLT_return int glt_can_future_functions()
+GLT_func_prefix int glt_can_future_functions()
 {
 #ifdef CORE
     return 0;
@@ -23,29 +23,29 @@ GLT_return int glt_can_future_functions()
 
 #ifndef CORE
 
-GLT_return void glt_future_create (int nbytes, GLT_future *newfuture)
+GLT_func_prefix void glt_future_create (int nbytes, GLT_future *newfuture)
 {
     ABT_eventual_create (nbytes, newfuture);
 }
 
-GLT_return void glt_future_free (GLT_future *future)
+GLT_func_prefix void glt_future_free (GLT_future *future)
 {
     ABT_eventual_free (future);
 }
 
-GLT_return void glt_future_wait (GLT_future future, void **value)
+GLT_func_prefix void glt_future_wait (GLT_future future, void **value)
 {
     ABT_eventual_wait (future, value);
 }
 
-GLT_return void glt_future_set (GLT_future future, void *value, int nbytes)
+GLT_func_prefix void glt_future_set (GLT_future future, void *value, int nbytes)
 {
     ABT_eventual_set (future, value, nbytes);
 }
 #endif
 
 //promise functions
-GLT_return int glt_can_promise_functions()
+GLT_func_prefix int glt_can_promise_functions()
 {
 #ifdef CORE
     return 0;
@@ -56,27 +56,27 @@ GLT_return int glt_can_promise_functions()
 
 #ifndef CORE
 
-GLT_return void glt_promise_create (uint32_t compartments, void(*cb_func)(void **arg), GLT_promise *newpromise)
+GLT_func_prefix void glt_promise_create (uint32_t compartments, void(*cb_func)(void **arg), GLT_promise *newpromise)
 {
     ABT_future_create (compartments, cb_func, newpromise);
 }
 
-GLT_return void glt_promise_free (GLT_promise *promise)
+GLT_func_prefix void glt_promise_free (GLT_promise *promise)
 {
     ABT_future_free (promise);
 }
 
-GLT_return void glt_promise_wait (GLT_promise promise)
+GLT_func_prefix void glt_promise_wait (GLT_promise promise)
 {
     ABT_future_wait (promise);
 }
 
-GLT_return void glt_promise_test (GLT_promise promise, GLT_bool *flag)
+GLT_func_prefix void glt_promise_test (GLT_promise promise, GLT_bool *flag)
 {
     ABT_future_test (promise,flag);
 }
 
-GLT_return void glt_promise_set (GLT_promise promise, void * value)
+GLT_func_prefix void glt_promise_set (GLT_promise promise, void * value)
 {
     ABT_future_set (promise,value);
 }
