bc1d93fede5aa56880d5e9c2a882500b7ae6beae
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:58 +0000

    qthreadss glt_util.c file updated with new prefix

--- src/qthreads/glt_util.c
+++ src/qthreads/glt_util.c
@@ -1,21 +1,21 @@
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
-GLT_return int glt_get_num_threads() 
+GLT_func_prefix int glt_get_num_threads() 
 {
     return main_team->num_shepherds;
 }
 
-GLT_return int glt_get_thread_num() 
+GLT_func_prefix int glt_get_thread_num() 
 {
     return qthread_shep();
 }
 
-GLT_return int glt_can_util_functions()
+GLT_func_prefix int glt_can_util_functions()
 {
 #ifdef CORE
     return 0;
@@ -26,73 +26,73 @@ GLT_return int glt_can_util_functions()
 
 #ifndef CORE
 
- GLT_return void glt_util_double_max(double * res, double * array, 
+ GLT_func_prefix void glt_util_double_max(double * res, double * array, 
         size_t lenght, int checkfeb){
     *res = qutil_double_max(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_uint_max(unsigned int * res, 
+GLT_func_prefix void glt_util_uint_max(unsigned int * res, 
         const long unsigned int * array, size_t lenght, int checkfeb){
     *res = qutil_uint_max(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_int_max(int * res, const long int * array, 
+GLT_func_prefix void glt_util_int_max(int * res, const long int * array, 
         size_t lenght, int checkfeb){
     *res = qutil_int_max(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_double_min(double * res, double * array, 
+GLT_func_prefix void glt_util_double_min(double * res, double * array, 
         size_t lenght, int checkfeb){
     *res = qutil_double_min(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_uint_min(unsigned int * res, 
+GLT_func_prefix void glt_util_uint_min(unsigned int * res, 
         const long unsigned int * array, size_t lenght, int checkfeb){
     *res = qutil_uint_min(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_int_min(int * res, const long int * array, 
+GLT_func_prefix void glt_util_int_min(int * res, const long int * array, 
         size_t lenght, int checkfeb){
     *res = qutil_int_min(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_double_mult(double * res, double * array, 
+GLT_func_prefix void glt_util_double_mult(double * res, double * array, 
         size_t lenght, int checkfeb){
     *res = qutil_double_mult(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_uint_mult(unsigned int * res, 
+GLT_func_prefix void glt_util_uint_mult(unsigned int * res, 
         const long unsigned int * array, size_t lenght, int checkfeb){
     *res = qutil_uint_mult(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_int_mult(int * res, const long int * array, 
+GLT_func_prefix void glt_util_int_mult(int * res, const long int * array, 
         size_t lenght, int checkfeb){
     *res = qutil_int_mult(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_double_sum(double * res, double * array, 
+GLT_func_prefix void glt_util_double_sum(double * res, double * array, 
         size_t lenght, int checkfeb){
     *res = qutil_double_sum(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_uint_sum(unsigned int * res, 
+GLT_func_prefix void glt_util_uint_sum(unsigned int * res, 
         const long unsigned int * array, size_t lenght, 
         int checkfeb){
     *res = qutil_uint_sum(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_int_sum(int * res, const long int * array, size_t lenght, 
+GLT_func_prefix void glt_util_int_sum(int * res, const long int * array, size_t lenght, 
         int checkfeb){
     *res = qutil_int_sum(array,lenght, checkfeb);
 }
 
-GLT_return void glt_util_qsort(double * array, size_t lenght)
+GLT_func_prefix void glt_util_qsort(double * array, size_t lenght)
 {
     qutil_qsort(array,lenght);
 }
 
-GLT_return void glt_util_mergesort(double * array, size_t lenght)
+GLT_func_prefix void glt_util_mergesort(double * array, size_t lenght)
 {
     qutil_mergesort(array,lenght);
 }
