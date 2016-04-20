98545e12b3e3107e36a850af508c82095012307b
Adrian Castello <adcastel@uji.es>
2016-04-20 13:07:02 +0000

    massivethreads glt_prof_log.c file updated with new prefix

--- src/massivethreads/glt_prof_log.c
+++ src/massivethreads/glt_prof_log.c
@@ -4,9 +4,14 @@
  * and open the template in the editor.
  */
 
+#ifndef FASTGLT
 #include <glt.h>
+#define GLT_func_prefix 
+#else
+#define GLT_func_prefix static inline
+#endif
 
- int glt_can_log_functions()
+GLT_func_prefix int glt_can_log_functions()
 {
 #ifdef CORE
     return 0;
@@ -16,32 +21,32 @@
 }
 
 #ifndef CORE
- void glt_log_start()
+GLT_func_prefix void glt_log_start()
 {
     myth_log_start();
 }
 
- void glt_log_pause()
+GLT_func_prefix void glt_log_pause()
 {
     myth_log_pause();
 }
 
- void glt_log_flush()
+GLT_func_prefix void glt_log_flush()
 {
     myth_log_flush();
 }
- void glt_log_reset()
+GLT_func_prefix void glt_log_reset()
 {
     myth_log_reset();
 }
 
- void glt_log_annotate_ult(GLT_ult ult, char *name)
+GLT_func_prefix void glt_log_annotate_ult(GLT_ult ult, char *name)
 {
     myth_log_annotate_thread(ult,name);
 }
 #endif
 
- int glt_can_prof_functions()
+GLT_func_prefix int glt_can_prof_functions()
 {
 #ifdef CORE
     return 0;
@@ -51,12 +56,12 @@
 }
 
 #ifndef CORE
- void glt_sched_prof_start()
+GLT_func_prefix void glt_sched_prof_start()
 {
     myth_sched_prof_start();
 }
 
- void glt_sched_prof_pause()
+GLT_func_prefix void glt_sched_prof_pause()
 {
     myth_sched_prof_pause();
 }
