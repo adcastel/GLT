669643db6c846caca692a89f9051a1560f1dd1fd
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:57 +0000

    qthreadss glt_memory.c file updated with new prefix

--- src/qthreads/glt_memory.c
+++ src/qthreads/glt_memory.c
@@ -6,12 +6,12 @@
 
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
-GLT_return int glt_can_memory_functions()
+GLT_func_prefix int glt_can_memory_functions()
 {
 #ifdef CORE
     return 0;
@@ -22,59 +22,59 @@ GLT_return int glt_can_memory_functions()
 
 #ifndef CORE
 
-GLT_return void * glt_memory_make_stat_map(const off_t filesize, void *addr,
+GLT_func_prefix void * glt_memory_make_stat_map(const off_t filesize, void *addr,
         const char *filename, size_t itemsize, const size_t streams)
 {
     return qalloc_makestatmap (filesize, addr, filename, itemsize, streams);
 }
 
-GLT_return void * glt_memory_make_dyn_map(const off_t filesize, void *addr,
+GLT_func_prefix void * glt_memory_make_dyn_map(const off_t filesize, void *addr,
         const char *filename,  const size_t streams)
 {
     return qalloc_makedynmap (filesize, addr, filename, streams);
 }
 
-GLT_return void * glt_memory_load_map(const char *filename)
+GLT_func_prefix void * glt_memory_load_map(const char *filename)
 {
     return qalloc_loadmap (filename);
 }
 
-GLT_return void * glt_memory_malloc(void *map, size_t size)
+GLT_func_prefix void * glt_memory_malloc(void *map, size_t size)
 {
     return qalloc_malloc (map,size);
 }
 
-GLT_return void * glt_memory_stat_malloc(struct mapinfo_s *map)
+GLT_func_prefix void * glt_memory_stat_malloc(struct mapinfo_s *map)
 {
     return qalloc_statmalloc (map);
 }
 
-GLT_return void * glt_memory_dyn_malloc(struct dynmapinfo_s *map, size_t size)
+GLT_func_prefix void * glt_memory_dyn_malloc(struct dynmapinfo_s *map, size_t size)
 {
     return qalloc_dynmalloc (map, size);
 }
 
-GLT_return void glt_memory_free(void *block, void * map)
+GLT_func_prefix void glt_memory_free(void *block, void * map)
 {
      qalloc_free (block,map);
 }
 
-GLT_return void glt_memory_stat_free(void *block, struct mapinfo_s *map)
+GLT_func_prefix void glt_memory_stat_free(void *block, struct mapinfo_s *map)
 {
      qalloc_statfree (block,map);
 }
 
-GLT_return void glt_memory_dyn_free(void *block, struct dynmapinfo_s *map)
+GLT_func_prefix void glt_memory_dyn_free(void *block, struct dynmapinfo_s *map)
 {
      qalloc_dynfree (block,map);
 }
 
-GLT_return void glt_memory_cleanup()
+GLT_func_prefix void glt_memory_cleanup()
 {
     qalloc_cleanup();
 }
 
-GLT_return void glt_memory_checkpoint()
+GLT_func_prefix void glt_memory_checkpoint()
 {
     qalloc_checkpoint();
 }
