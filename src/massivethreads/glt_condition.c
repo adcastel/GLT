ec14833be2c556a42e44bf83f5faed6ca15df66a
Adrian Castello <adcastel@uji.es>
2016-04-20 13:07:02 +0000

    massivethreads glt_condition.c file updated with new prefix

--- src/massivethreads/glt_condition.c
+++ src/massivethreads/glt_condition.c
@@ -1,26 +1,31 @@
+#ifndef FASTGLT
 #include <glt.h>
+#define GLT_func_prefix 
+#else
+#define GLT_func_prefix static inline
+#endif
 
-void glt_cond_create(GLT_cond *cond)
+GLT_func_prefix void glt_cond_create(GLT_cond *cond)
 {
     *cond =  myth_cond_create();
 }
 
-void glt_cond_free(GLT_cond *cond)
+GLT_func_prefix void glt_cond_free(GLT_cond *cond)
 {
     myth_cond_destroy(*cond);
 }
 
-void glt_cond_signal(GLT_cond cond)
+GLT_func_prefix void glt_cond_signal(GLT_cond cond)
 {
     myth_cond_signal(cond);
 }
 
-void glt_cond_wait(GLT_cond cond, GLT_mutex mutex)
+GLT_func_prefix void glt_cond_wait(GLT_cond cond, GLT_mutex mutex)
 {
     myth_cond_wait(cond,mutex);
 }
 
-void glt_cond_broadcast(GLT_cond cond)
+GLT_func_prefix void glt_cond_broadcast(GLT_cond cond)
 {
     myth_cond_broadcast(cond);
 }
