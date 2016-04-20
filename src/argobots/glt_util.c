7da0ced5f94378c8601133ab476901bcc2fe8aa9
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:44 +0000

    argobots glt_util.c file updated with new prefix

--- src/argobots/glt_util.c
+++ src/argobots/glt_util.c
@@ -1,16 +1,16 @@
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
-GLT_return int glt_get_num_threads() 
+GLT_func_prefix int glt_get_num_threads() 
 {
     return main_team->num_xstreams;
 }
 
-GLT_return int glt_get_thread_num() 
+GLT_func_prefix int glt_get_thread_num() 
 {
     int rank;
     ABT_xstream_self_rank(&rank);
