fb42c5794e2707b9bb6650f889f6a3e2adae69a8
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:57 +0000

    qthreadss glt_mutex.c file updated with new prefix

--- src/qthreads/glt_mutex.c
+++ src/qthreads/glt_mutex.c
@@ -1,30 +1,30 @@
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
-GLT_return void glt_mutex_create(GLT_mutex * mutex)
+GLT_func_prefix void glt_mutex_create(GLT_mutex * mutex)
 {
    // mutex = (GLT_mutex *)malloc(sizeof(GLT_mutex));
 }
 
-GLT_return void glt_mutex_lock(GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_lock(GLT_mutex mutex)
 {
     qthread_lock(mutex);
 }
 
-GLT_return void glt_mutex_unlock(GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_unlock(GLT_mutex mutex)
 {
     qthread_unlock(mutex);
 }
 
-GLT_return void glt_mutex_free(GLT_mutex * mutex)
+GLT_func_prefix void glt_mutex_free(GLT_mutex * mutex)
 {
     //free(mutex);
 }
 
-GLT_return void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex)
 {
     aligned_t res = qthread_cas(mutex,0,1);
     *locked = (res==0)? 1 : 0;        
