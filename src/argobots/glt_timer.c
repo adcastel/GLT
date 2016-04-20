b5226c30a4ee8c60a43d26b7648ff6b4b5863e77
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:44 +0000

    argobots glt_timer.c file updated with new prefix

--- src/argobots/glt_timer.c
+++ src/argobots/glt_timer.c
@@ -1,36 +1,36 @@
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
-GLT_return double glt_get_wtime() 
+GLT_func_prefix double glt_get_wtime() 
 {
     return ABT_get_wtime();
 }
 
-GLT_return void glt_timer_create(GLT_timer * timer) 
+GLT_func_prefix void glt_timer_create(GLT_timer * timer) 
 {
     ABT_timer_create(timer);
 }
 
-GLT_return void glt_timer_free(GLT_timer * timer) 
+GLT_func_prefix void glt_timer_free(GLT_timer * timer) 
 {
     ABT_timer_free(timer);
 }
 
-GLT_return void glt_timer_start(GLT_timer timer) 
+GLT_func_prefix void glt_timer_start(GLT_timer timer) 
 {
     ABT_timer_start(timer);
 }
 
-GLT_return void glt_timer_stop(GLT_timer timer) 
+GLT_func_prefix void glt_timer_stop(GLT_timer timer) 
 {
     ABT_timer_stop(timer);
 }
 
-GLT_return void glt_timer_get_secs(GLT_timer timer, double *secs) 
+GLT_func_prefix void glt_timer_get_secs(GLT_timer timer, double *secs) 
 {
     ABT_timer_read(timer,secs);
 }
