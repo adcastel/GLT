95edf88064350c88c9c0704464d831d58f656d73
Adrian Castello <adcastel@uji.es>
2016-04-20 13:07:02 +0000

    massivethreads glt_mutex.c file updated with new prefix

--- src/massivethreads/glt_mutex.c
+++ src/massivethreads/glt_mutex.c
@@ -1,24 +1,31 @@
+#ifndef FASTGLT
 #include <glt.h>
+#define GLT_func_prefix 
+#else
+#define GLT_func_prefix static inline
+#endif
 
-void glt_mutex_create(GLT_mutex * mutex)
+GLT_func_prefix void glt_mutex_create(GLT_mutex * mutex)
 {
     *mutex = myth_mutex_create();
 }
-void glt_mutex_lock(GLT_mutex mutex)
+
+GLT_func_prefix void glt_mutex_lock(GLT_mutex mutex)
 {
     myth_mutex_lock(mutex);
 }
-void glt_mutex_unlock(GLT_mutex mutex)
+
+GLT_func_prefix void glt_mutex_unlock(GLT_mutex mutex)
 {
     myth_mutex_unlock(mutex);
 }
 
-void glt_mutex_free(GLT_mutex * mutex)
+GLT_func_prefix void glt_mutex_free(GLT_mutex * mutex)
 {
     myth_mutex_destroy(*mutex);
 }
 
-void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex)
+GLT_func_prefix void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex)
 {
     *locked = myth_mutex_trylock(mutex);
 }
