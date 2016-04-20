e1d01403963560680272b31de646f1b6002bdfa3
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:44 +0000

    argobots glt_scheduler.c file updated with new prefix

--- src/argobots/glt_scheduler.c
+++ src/argobots/glt_scheduler.c
@@ -6,15 +6,15 @@
 
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
 //scheduler functions
 
 
-GLT_return int glt_can_manage_scheduler()
+GLT_func_prefix int glt_can_manage_scheduler()
 {
 #ifdef CORE
     return 0;
@@ -41,71 +41,71 @@ GLT_return int glt_can_manage_scheduler()
     ABT_sched_config_read (config, num_vars,...);
 }*/
 
-GLT_return void glt_scheduler_config_free (GLT_sched_config *config)
+GLT_func_prefix void glt_scheduler_config_free (GLT_sched_config *config)
 { 	
     ABT_sched_config_free (config);
 }
 
-GLT_return void glt_scheduler_create (GLT_sched_def *def, int num_pools, 
+GLT_func_prefix void glt_scheduler_create (GLT_sched_def *def, int num_pools, 
         GLT_pool *pools, GLT_sched_config config, GLT_sched *newsched)
 {
     ABT_sched_create (def, num_pools, pools, config, newsched);
 }
 
-GLT_return void glt_schededuler_create_basic (GLT_sched_predef predef, 
+GLT_func_prefix void glt_schededuler_create_basic (GLT_sched_predef predef, 
         int num_pools, GLT_pool *pools, GLT_sched_config config, 
         GLT_sched *newsched)
 {
     ABT_sched_create_basic (predef, num_pools, pools, config, newsched);
 }
 
-GLT_return void glt_scheduler_free (GLT_sched *sched)
+GLT_func_prefix void glt_scheduler_free (GLT_sched *sched)
 {
     ABT_sched_free (sched);
 }
 
-GLT_return void glt_scheduler_get_num_pools(GLT_sched sched, int *num_pools)
+GLT_func_prefix void glt_scheduler_get_num_pools(GLT_sched sched, int *num_pools)
 {
     ABT_sched_get_num_pools ( sched, num_pools);
 }
 
-GLT_return void glt_scheduler_get_pools(GLT_sched sched, int max_pools, 
+GLT_func_prefix void glt_scheduler_get_pools(GLT_sched sched, int max_pools, 
         int idx, GLT_pool *pools)
 {
     ABT_sched_get_pools ( sched, max_pools, idx, pools);
 }
 
-GLT_return void glt_scheduler_finish (GLT_sched sched)
+GLT_func_prefix void glt_scheduler_finish (GLT_sched sched)
 {
     ABT_sched_finish (sched);
 }
 
-GLT_return void glt_scheduler_exit (GLT_sched sched)
+GLT_func_prefix void glt_scheduler_exit (GLT_sched sched)
 {
     ABT_sched_exit (sched);
 }
 
-GLT_return void glt_scheduler_has_to_stop (GLT_sched sched, GLT_bool *stop)
+GLT_func_prefix void glt_scheduler_has_to_stop (GLT_sched sched, GLT_bool *stop)
 {
     ABT_sched_has_to_stop (sched, stop);
 }
 
-GLT_return void glt_scheduler_set_data (GLT_sched sched, void *data)
+GLT_func_prefix void glt_scheduler_set_data (GLT_sched sched, void *data)
 {
     ABT_sched_set_data (sched,data);
 }
 
-GLT_return void glt_scheduler_get_data(GLT_sched sched, void **data)
+GLT_func_prefix void glt_scheduler_get_data(GLT_sched sched, void **data)
 {
     ABT_sched_get_data (sched, data);
 }
 
-GLT_return void glt_scheduler_get_size (GLT_sched sched, size_t *size)
+GLT_func_prefix void glt_scheduler_get_size (GLT_sched sched, size_t *size)
 {
     ABT_sched_get_size (sched, size);
 }
 
-GLT_return void glt_scheduler_get_total_size(GLT_sched sched, size_t *size)
+GLT_func_prefix void glt_scheduler_get_total_size(GLT_sched sched, size_t *size)
 {
     ABT_sched_get_total_size (sched, size);
 }
