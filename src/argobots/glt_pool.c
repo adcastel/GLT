f1ff8bfc31b88a8357e15fc6fe6d770bde710419
Adrian Castello <adcastel@uji.es>
2016-04-20 12:32:43 +0000

    argobots glt_pool.c file updated with new prefix

--- src/argobots/glt_pool.c
+++ src/argobots/glt_pool.c
@@ -6,14 +6,14 @@
 
 #ifndef FASTGLT
 #include <glt.h>
-#define GLT_return 
+#define GLT_func_prefix 
 #else
-#define GLT_return static inline
+#define GLT_func_prefix static inline
 #endif
 
 //pools functions
 
-GLT_return int glt_can_manage_pools()
+GLT_func_prefix int glt_can_manage_pools()
 {
 #ifdef CORE
     return 0;
@@ -23,7 +23,7 @@ GLT_return int glt_can_manage_pools()
 }
 #ifndef CORE
 
-GLT_return void glt_pool_create (GLT_pool_def *def, GLT_pool_config config, 
+GLT_func_prefix void glt_pool_create (GLT_pool_def *def, GLT_pool_config config, 
         GLT_pool *newpool)
 {
     if(main_team->num_pools < main_team->max_elem){
@@ -36,7 +36,7 @@ GLT_return void glt_pool_create (GLT_pool_def *def, GLT_pool_config config,
     }
 }
 
-GLT_return void glt_pool_create_basic (GLT_pool_kind kind, 
+GLT_func_prefix void glt_pool_create_basic (GLT_pool_kind kind, 
         GLT_pool_access access, GLT_bool automatic, GLT_pool *newpool)
 {
     if(main_team->num_pools < main_team->max_elem){
@@ -49,57 +49,57 @@ GLT_return void glt_pool_create_basic (GLT_pool_kind kind,
     }
 }
 
-GLT_return void glt_pool_free (GLT_pool *pool)
+GLT_func_prefix void glt_pool_free (GLT_pool *pool)
 {
     ABT_pool_free (pool);
 }
 
-GLT_return  void glt_pool_get_access (GLT_pool pool, GLT_pool_access *access)
+GLT_func_prefix  void glt_pool_get_access (GLT_pool pool, GLT_pool_access *access)
 {
     ABT_pool_get_access (pool, access);
 }
 
-GLT_return void glt_pool_get_total_size (GLT_pool pool, size_t *size)
+GLT_func_prefix void glt_pool_get_total_size (GLT_pool pool, size_t *size)
 {
     ABT_pool_get_total_size (pool, size);
 }
 
-GLT_return void glt_pool_get_size (GLT_pool pool, size_t *size)
+GLT_func_prefix void glt_pool_get_size (GLT_pool pool, size_t *size)
 {
     ABT_pool_get_size (pool, size);
 }
 
-GLT_return void glt_pool_pop (GLT_pool pool, GLT_unit *p_unit)
+GLT_func_prefix void glt_pool_pop (GLT_pool pool, GLT_unit *p_unit)
 {
     ABT_pool_pop (pool,p_unit);
 }
 
-GLT_return void glt_pool_push (GLT_pool pool, GLT_unit p_unit)
+GLT_func_prefix void glt_pool_push (GLT_pool pool, GLT_unit p_unit)
 {
     ABT_pool_push (pool,p_unit);
 }
 
-GLT_return void glt_pool_remove (GLT_pool pool, GLT_unit p_unit)
+GLT_func_prefix void glt_pool_remove (GLT_pool pool, GLT_unit p_unit)
 {
     ABT_pool_remove (pool,p_unit);
 }
 
-GLT_return void glt_pool_set_data (GLT_pool pool, void *data)
+GLT_func_prefix void glt_pool_set_data (GLT_pool pool, void *data)
 {
     ABT_pool_set_data (pool,data);
 }
 
-GLT_return void glt_pool_get_data (GLT_pool pool, void **data)
+GLT_func_prefix void glt_pool_get_data (GLT_pool pool, void **data)
 {
     ABT_pool_get_data (pool,data);
 }
 
-GLT_return void glt_pool_add_sched (GLT_pool pool, GLT_sched sched)
+GLT_func_prefix void glt_pool_add_sched (GLT_pool pool, GLT_sched sched)
 {
     ABT_pool_add_sched (pool, sched);
 }
 
-GLT_return void glt_pool_get_id (GLT_pool pool, int *id)
+GLT_func_prefix void glt_pool_get_id (GLT_pool pool, int *id)
 {
     ABT_pool_get_id (pool, id);
 }
