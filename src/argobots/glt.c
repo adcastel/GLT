1f87add35c7505d97a67e72bf3b205478bda08a6
Adrian Castello <adcastel@uji.es>
2016-04-21 10:27:44 +0000

    glt.c from argobots cleaned

--- src/argobots/glt.c
+++ src/argobots/glt.c
@@ -5,20 +5,16 @@
 #define GLT_func_prefix static inline
 #endif
 
-
-GLT_func_prefix void glt_start() 
-{
+GLT_func_prefix void glt_start() {
     printf("Starting with ARGOBOTS\n");
 }
 
-GLT_func_prefix void glt_end() 
-{
+GLT_func_prefix void glt_end() {
     printf("Ending with ARGOBOTS\n");
 }
 
-GLT_func_prefix void glt_init(int argc, char * argv[]) 
-{
-     int num_threads = get_nprocs();
+GLT_func_prefix void glt_init(int argc, char * argv[]) {
+    int num_threads = get_nprocs();
 
     main_team = (glt_team_t *) malloc(sizeof (glt_team_t));
     ABT_init(argc, argv);
@@ -37,19 +33,19 @@ GLT_func_prefix void glt_init(int argc, char * argv[])
 
     main_team->num_xstreams = num_threads;
     main_team->num_pools = num_pools;
-    main_team->max_elem= get_nprocs();
+    main_team->max_elem = get_nprocs();
     main_team->team = (ABT_xstream *) malloc(sizeof (ABT_xstream) * main_team->max_elem);
     main_team->pools = (ABT_pool *) malloc(sizeof (ABT_pool) * main_team->max_elem);
 
     for (int i = 0; i < num_pools; i++) {
         ABT_pool_create_basic(ABT_POOL_FIFO, ABT_POOL_ACCESS_MPMC, ABT_TRUE,
-            &main_team->pools[i]);
+                &main_team->pools[i]);
     }
 
     ABT_xstream_self(&main_team->team[0]);
     ABT_xstream_set_main_sched_basic(main_team->team[0], ABT_SCHED_DEFAULT,
             1, &main_team->pools[0]);
-    
+
     for (int i = 1; i < num_threads; i++) {
         ABT_xstream_create_basic(ABT_SCHED_DEFAULT, 1,
                 &main_team->pools[i % main_team->num_pools],
@@ -58,8 +54,7 @@ GLT_func_prefix void glt_init(int argc, char * argv[])
     }
 }
 
-GLT_func_prefix void glt_finalize() 
-{
+GLT_func_prefix void glt_finalize() {
     for (int i = 1; i < main_team->num_xstreams; i++) {
         ABT_xstream_join(main_team->team[i]);
         ABT_xstream_free(&main_team->team[i]);
