d1254fb1cf72dc301789e19bdc911fb3c52a5598
Adrian Castello <adcastel@uji.es>
2016-04-21 10:27:44 +0000

    glt_event.c from argobots cleaned

--- src/argobots/glt_event.c
+++ src/argobots/glt_event.c
@@ -12,8 +12,7 @@
 #endif
 
 //Event functions
-GLT_func_prefix int glt_can_event_functions()
-{
+GLT_func_prefix int glt_can_event_functions() {
 #ifdef CORE
     return 0;
 #else
@@ -22,21 +21,18 @@ GLT_func_prefix int glt_can_event_functions()
 }
 
 #ifndef CORE
-GLT_func_prefix void glt_get_error_str(int errno, char *str, size_t *len)
-{
-    ABT_error_get_str (errno,str,len);
+GLT_func_prefix void glt_get_error_str(int errno, char *str, size_t *len) {
+    ABT_error_get_str(errno, str, len);
 }
 
-GLT_func_prefix void glt_event_add_callbac (GLT_event_kind event, GLT_event_cb_fn ask_cb,
-        void *ask_user_arg, GLT_event_cb_fn act_cb, void *act_user_arg, 
-        int *cb_id)
-{
-    ABT_event_add_callback (event, ask_cb, ask_user_arg, act_cb, 
+GLT_func_prefix void glt_event_add_callbac(GLT_event_kind event, GLT_event_cb_fn ask_cb,
+        void *ask_user_arg, GLT_event_cb_fn act_cb, void *act_user_arg,
+        int *cb_id) {
+    ABT_event_add_callback(event, ask_cb, ask_user_arg, act_cb,
             act_user_arg, cb_id);
 }
 
-GLT_func_prefix void glt_event_del_callback (GLT_event_kind event, int cb_id)
-{
-    ABT_event_del_callback (event, cb_id);
+GLT_func_prefix void glt_event_del_callback(GLT_event_kind event, int cb_id) {
+    ABT_event_del_callback(event, cb_id);
 }
 #endif
