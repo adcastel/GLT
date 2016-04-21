ee9d6761b3e0c107d19d028ff63e444330d0a052
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:41 +0000

    glt_barrier.c from massivethreads cleaned

--- src/massivethreads/glt_barrier.c
+++ src/massivethreads/glt_barrier.c
@@ -5,19 +5,14 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix void glt_barrier_create(int num_waiters, GLT_barrier *barrier)
-{
+GLT_func_prefix void glt_barrier_create(int num_waiters, GLT_barrier *barrier) {
     *barrier = myth_barrier_create(num_waiters);
 }
 
-GLT_func_prefix void glt_barrier_free(GLT_barrier *barrier)
-{
+GLT_func_prefix void glt_barrier_free(GLT_barrier *barrier) {
     myth_barrier_destroy(*barrier);
 }
 
-GLT_func_prefix void glt_barrier_wait(GLT_barrier *barrier)
-{
+GLT_func_prefix void glt_barrier_wait(GLT_barrier *barrier) {
     myth_barrier_wait(*barrier);
 }
-
-
