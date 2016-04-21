38f296cb9abadb25a6612a0fa2cae6329bc18d85
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:42 +0000

    glt_atomic.c from qthreads cleaned

--- src/qthreads/glt_atomic.c
+++ src/qthreads/glt_atomic.c
@@ -11,8 +11,7 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix int glt_can_atomic_functions()
-{
+GLT_func_prefix int glt_can_atomic_functions() {
 #ifdef CORE
     return 0;
 #else
@@ -22,31 +21,26 @@ GLT_func_prefix int glt_can_atomic_functions()
 
 #ifndef CORE
 
- GLT_func_prefix void glt_atomic_incr(GLT_aligned * operand, int incr)
-{
-    qthread_incr(operand,incr);
+GLT_func_prefix void glt_atomic_incr(GLT_aligned * operand, int incr) {
+    qthread_incr(operand, incr);
 }
 
-GLT_func_prefix void glt_atomic_fincr(float * operand, float incr)
-{
-    qthread_fincr(operand,incr);
+GLT_func_prefix void glt_atomic_fincr(float * operand, float incr) {
+    qthread_fincr(operand, incr);
 }
 
-GLT_func_prefix void glt_atomic_dincr(double * operand, double incr)
-{
-    qthread_dincr(operand,incr);
+GLT_func_prefix void glt_atomic_dincr(double * operand, double incr) {
+    qthread_dincr(operand, incr);
 }
 
 GLT_func_prefix void glt_atomic_cas(volatile GLT_aligned * addr, GLT_aligned oldval,
-        GLT_aligned newval)
-{
-    qthread_cas(addr,oldval,newval);
+        GLT_aligned newval) {
+    qthread_cas(addr, oldval, newval);
 }
 
 GLT_func_prefix void glt_atomic_cas_ptr(void * volatile * addr, void * oldval,
-        void * newval)
-{
-    qthread_cas_ptr(addr,oldval,newval);
+        void * newval) {
+    qthread_cas_ptr(addr, oldval, newval);
 }
 
 #endif
