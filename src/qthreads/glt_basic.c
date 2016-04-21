f3800bda05b807a57e294628cfdc3b88339d21ed
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:43 +0000

    glt_basic.c from qthreads cleaned

--- src/qthreads/glt_basic.c
+++ src/qthreads/glt_basic.c
@@ -11,8 +11,7 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix int glt_can_extended_basic()
-{
+GLT_func_prefix int glt_can_extended_basic() {
 #ifdef CORE
     return 0;
 #else
@@ -22,41 +21,33 @@ GLT_func_prefix int glt_can_extended_basic()
 
 #ifndef CORE
 
-
-
-
 GLT_func_prefix void glt_ult_creation_syncvar(void(*thread_func)(void *), void * arg,
-        GLT_syncvar *syncvar){
-    qthread_fork_syncvar((void *)thread_func,arg,syncvar);
+        GLT_syncvar *syncvar) {
+    qthread_fork_syncvar((void *) thread_func, arg, syncvar);
 }
 
 GLT_func_prefix void glt_ult_creation_syncvar_to(void(*thread_func)(void *), void * arg,
-        GLT_syncvar *syncvar, int dest,int npreconds, ...){
-    qthread_fork_syncvar_to((void *)thread_func,arg,syncvar,dest);
+        GLT_syncvar *syncvar, int dest, int npreconds, ...) {
+    qthread_fork_syncvar_to((void *) thread_func, arg, syncvar, dest);
 }
 
-GLT_func_prefix void glt_ult_get_thread(GLT_thread *thread, int *worker)
-{
+GLT_func_prefix void glt_ult_get_thread(GLT_thread *thread, int *worker) {
     *worker = qthread_worker(thread);
 }
 
-GLT_func_prefix void glt_ult_get_thread_unique(GLT_thread *thread, int *worker)
-{
+GLT_func_prefix void glt_ult_get_thread_unique(GLT_thread *thread, int *worker) {
     *worker = qthread_worker_unique(thread);
 }
 
-GLT_func_prefix void glt_ult_get_tasklocal(void * pointer, unsigned int size)
-{
+GLT_func_prefix void glt_ult_get_tasklocal(void * pointer, unsigned int size) {
     pointer = qthread_get_tasklocal(size);
 }
 
-GLT_func_prefix void glt_ult_size_tasklocal(unsigned int *size)
-{
+GLT_func_prefix void glt_ult_size_tasklocal(unsigned int *size) {
     *size = qthread_size_tasklocal();
 }
 
-GLT_func_prefix void glt_ult_get_stack_left(size_t *size)
-{
+GLT_func_prefix void glt_ult_get_stack_left(size_t *size) {
     *size = qthread_stackleft();
 }
 
