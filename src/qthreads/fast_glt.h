3d06ef13133da516e55e7289f7fd78c8c90c1d65
Adrian Castello <adcastel@uji.es>
2016-04-21 10:57:01 +0000

    fast_glt.h from qthreads cleaned

--- src/qthreads/fast_glt.h
+++ src/qthreads/fast_glt.h
@@ -1,8 +1,6 @@
-/* 
- * File:   glt.h
- * Author: adcastel
- *
- * Created on 11 de febrero de 2016, 16:20
+/* See COPYRIGHT in glt_common.h file. */
+/*
+ * Includes .c files for the static library.
  */
 
 #ifndef FAST_GLT_H
