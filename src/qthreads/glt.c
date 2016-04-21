ba0fc8d80f9f3fac13ddb4b2f4721c1727801c25
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:43 +0000

    glt.c from qthreads cleaned

--- src/qthreads/glt.c
+++ src/qthreads/glt.c
@@ -5,32 +5,29 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix void glt_start() 
-{
+GLT_func_prefix void glt_start() {
     printf("Starting with QTHREADS\n");
 }
 
-GLT_func_prefix void glt_end() 
-{
+GLT_func_prefix void glt_end() {
     printf("Ending with QTHREADS\n");
 }
 
-GLT_func_prefix void glt_init(int argc, char * argv[]) 
-{
+GLT_func_prefix void glt_init(int argc, char * argv[]) {
     int num_threads = get_nprocs();
     main_team = (glt_team_t *) malloc(sizeof (glt_team_t));
 
     char buff[10];
     int num_workers_per_thread;
-    
+
     if (getenv("GLT_NUM_THREADS") != NULL) {
         num_threads = atoi(getenv("GLT_NUM_THREADS"));
         sprintf(buff, "%d", num_threads);
         setenv("QTHREAD_NUM_SHEPHERDS", buff, 1);
-    } else if (getenv("QTHREAD_NUM_SHEPHERDS") != NULL){
+    } else if (getenv("QTHREAD_NUM_SHEPHERDS") != NULL) {
         num_threads = atoi(getenv("QTHREAD_NUM_SHEPHERDS"));
     } else {
-        sprintf(buff,"%d",num_threads);
+        sprintf(buff, "%d", num_threads);
         setenv("QTHREAD_NUM_SHEPHERDS", buff, 1);
     }
 
@@ -40,22 +37,21 @@ GLT_func_prefix void glt_init(int argc, char * argv[])
         setenv("QTHREAD_NUM_WORKERS_PER_SHEPHERD", buff, 1);
     } else if (getenv("QTHREAD_NUM_WORKERS_PER_SHEPHERD") != NULL) {
         num_workers_per_thread = atoi(getenv("QTHREAD_NUM_WORKERS_PER_SHEPHERD"));
-    }
-    else {
+    } else {
         setenv("QTHREAD_NUM_WORKERS_PER_SHEPHERD", "1", 1);
     }
-    
+
     if (num_threads == 1 && num_workers_per_thread > 1) {
         setenv("QTHREAD_SHEPHERD_BOUNDARY", "node", 1);
         setenv("QTHREAD_WORKER_UNIT", "core", 1);
     }
-    
-    if (num_threads > 1 && num_threads <= get_nprocs()/2) {
+
+    if (num_threads > 1 && num_threads <= get_nprocs() / 2) {
         setenv("QTHREAD_SHEPHERD_BOUNDARY", "core", 1);
         setenv("QTHREAD_WORKER_UNIT", "core", 1);
     }
-    
-    if (num_threads > 1 && num_threads > get_nprocs()/2) {
+
+    if (num_threads > 1 && num_threads > get_nprocs() / 2) {
         setenv("QTHREAD_SHEPHERD_BOUNDARY", "pu", 1);
         setenv("QTHREAD_WORKER_UNIT", "pu", 1);
     }
@@ -66,8 +62,6 @@ GLT_func_prefix void glt_init(int argc, char * argv[])
     qthread_initialize(); //qthreads
 }
 
-GLT_func_prefix void glt_finalize() 
-{
+GLT_func_prefix void glt_finalize() {
     qthread_finalize();
 }
-
