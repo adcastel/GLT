5989cb5fbc06180bf719939d32d48f60855d0de1
Adrian Castello <adcastel@uji.es>
2016-04-21 10:27:45 +0000

    glt_tls.c from argobots cleaned

--- src/argobots/glt_tls.c
+++ src/argobots/glt_tls.c
@@ -12,8 +12,7 @@
 #endif
 
 //tls functions
-GLT_func_prefix int glt_can_tls_functions()
-{
+GLT_func_prefix int glt_can_tls_functions() {
 #ifdef CORE
     return 0;
 #else
@@ -23,23 +22,19 @@ GLT_func_prefix int glt_can_tls_functions()
 
 #ifndef CORE
 
-GLT_func_prefix void glt_key_create (void(*destructor)(void *value), GLT_key *newkey)
-{
-    ABT_key_create (destructor,newkey);
+GLT_func_prefix void glt_key_create(void(*destructor)(void *value), GLT_key *newkey) {
+    ABT_key_create(destructor, newkey);
 }
 
-GLT_func_prefix void glt_key_free (GLT_key *key)
-{
-    ABT_key_free (key);
+GLT_func_prefix void glt_key_free(GLT_key *key) {
+    ABT_key_free(key);
 }
 
-GLT_func_prefix void glt_key_set (GLT_key key, void *value)
-{
-    ABT_key_set (key,value);
+GLT_func_prefix void glt_key_set(GLT_key key, void *value) {
+    ABT_key_set(key, value);
 }
 
-GLT_func_prefix void glt_key_get (GLT_key key, void **value)
-{
-    ABT_key_get (key,value);
+GLT_func_prefix void glt_key_get(GLT_key key, void **value) {
+    ABT_key_get(key, value);
 }
 #endif
