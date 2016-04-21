20bfe5fce0f9efde6518e3b6a2f3b94bc7a0e8f4
Adrian Castello <adcastel@uji.es>
2016-04-21 10:27:45 +0000

    glt_self.c from argobots cleaned

--- src/argobots/glt_self.c
+++ src/argobots/glt_self.c
@@ -10,10 +10,9 @@
 #else
 #define GLT_func_prefix static inline
 #endif
-//self functions
 
-GLT_func_prefix int glt_can_self()
-{
+//self functions
+GLT_func_prefix int glt_can_self() {
 #ifdef CORE
     return 0;
 #else
@@ -23,34 +22,28 @@ GLT_func_prefix int glt_can_self()
 
 #ifndef CORE
 
-GLT_func_prefix void glt_self_get_type (GLT_unit_type *type)
-{
-    ABT_self_get_type (type);
+GLT_func_prefix void glt_self_get_type(GLT_unit_type *type) {
+    ABT_self_get_type(type);
 }
 
-GLT_func_prefix void glt_self_is_primary (GLT_bool *flag)
-{
-    ABT_self_is_primary (flag);
+GLT_func_prefix void glt_self_is_primary(GLT_bool *flag) {
+    ABT_self_is_primary(flag);
 }
 
-GLT_func_prefix void glt_self_on_primary_xstream (GLT_bool *flag)
-{
-    ABT_self_on_primary_xstream (flag);
+GLT_func_prefix void glt_self_on_primary_xstream(GLT_bool *flag) {
+    ABT_self_on_primary_xstream(flag);
 }
 
-GLT_func_prefix void glt_self_get_last_pool_id (int *pool_id)
-{
-    ABT_self_get_last_pool_id (pool_id);
+GLT_func_prefix void glt_self_get_last_pool_id(int *pool_id) {
+    ABT_self_get_last_pool_id(pool_id);
 }
 
-GLT_func_prefix void glt_self_suspend (void)
-{
-    ABT_self_suspend ();
+GLT_func_prefix void glt_self_suspend(void) {
+    ABT_self_suspend();
 }
 
-GLT_func_prefix void glt_self_get_arg (void **arg)
-{
-    ABT_self_get_arg (arg);
+GLT_func_prefix void glt_self_get_arg(void **arg) {
+    ABT_self_get_arg(arg);
 }
 
 #endif
