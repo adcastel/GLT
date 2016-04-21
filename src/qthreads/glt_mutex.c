9c34d92fd0b0543918a87504ce7332a04da58a01
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:43 +0000

    glt_mutex.c from qthreads cleaned

--- src/qthreads/glt_mutex.c
+++ src/qthreads/glt_mutex.c
@@ -4,28 +4,24 @@
 #else
 #define GLT_func_prefix static inline
 #endif
-GLT_func_prefix void glt_mutex_create(GLT_mutex * mutex)
-{
-   // mutex = (GLT_mutex *)malloc(sizeof(GLT_mutex));
+
+GLT_func_prefix void glt_mutex_create(GLT_mutex * mutex) {
+    // mutex = (GLT_mutex *)malloc(sizeof(GLT_mutex));
 }
 
-GLT_func_prefix void glt_mutex_lock(GLT_mutex mutex)
-{
+GLT_func_prefix void glt_mutex_lock(GLT_mutex mutex) {
     qthread_lock(mutex);
 }
 
-GLT_func_prefix void glt_mutex_unlock(GLT_mutex mutex)
-{
+GLT_func_prefix void glt_mutex_unlock(GLT_mutex mutex) {
     qthread_unlock(mutex);
 }
 
-GLT_func_prefix void glt_mutex_free(GLT_mutex * mutex)
-{
+GLT_func_prefix void glt_mutex_free(GLT_mutex * mutex) {
     //free(mutex);
 }
 
-GLT_func_prefix void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex)
-{
-    aligned_t res = qthread_cas(mutex,0,1);
-    *locked = (res==0)? 1 : 0;        
+GLT_func_prefix void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex) {
+    aligned_t res = qthread_cas(mutex, 0, 1);
+    *locked = (res == 0) ? 1 : 0;
 }
