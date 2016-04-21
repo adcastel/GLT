c6183689d1f250875a829a2ac12247debecf109d
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:42 +0000

    glt_mutex.c from massivethreads cleaned

--- src/massivethreads/glt_mutex.c
+++ src/massivethreads/glt_mutex.c
@@ -5,27 +5,22 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix void glt_mutex_create(GLT_mutex * mutex)
-{
+GLT_func_prefix void glt_mutex_create(GLT_mutex * mutex) {
     *mutex = myth_mutex_create();
 }
 
-GLT_func_prefix void glt_mutex_lock(GLT_mutex mutex)
-{
+GLT_func_prefix void glt_mutex_lock(GLT_mutex mutex) {
     myth_mutex_lock(mutex);
 }
 
-GLT_func_prefix void glt_mutex_unlock(GLT_mutex mutex)
-{
+GLT_func_prefix void glt_mutex_unlock(GLT_mutex mutex) {
     myth_mutex_unlock(mutex);
 }
 
-GLT_func_prefix void glt_mutex_free(GLT_mutex * mutex)
-{
+GLT_func_prefix void glt_mutex_free(GLT_mutex * mutex) {
     myth_mutex_destroy(*mutex);
 }
 
-GLT_func_prefix void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex)
-{
+GLT_func_prefix void glt_mutex_trylock(GLT_bool * locked, GLT_mutex mutex) {
     *locked = myth_mutex_trylock(mutex);
 }
