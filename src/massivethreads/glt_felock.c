cd9448d4e60bf4f5a8ff058884c88f723aaa8232
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:41 +0000

    glt_felock.c from massivethreads cleaned

--- src/massivethreads/glt_felock.c
+++ src/massivethreads/glt_felock.c
@@ -11,7 +11,7 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix int can_felock_functions(){
+GLT_func_prefix int can_felock_functions() {
 #ifdef CORE
     return 0;
 #else
@@ -20,39 +20,33 @@ GLT_func_prefix int can_felock_functions(){
 }
 
 #ifndef CORE
-GLT_func_prefix void glt_felock_create(GLT_felock * felock)
-{
+
+GLT_func_prefix void glt_felock_create(GLT_felock * felock) {
     *felock = myth_felock_create();
 }
 
-GLT_func_prefix void glt_felock_free(GLT_felock felock)
-{
+GLT_func_prefix void glt_felock_free(GLT_felock felock) {
     myth_felock_destroy(felock);
 }
 
-GLT_func_prefix void glt_felock_lock(GLT_bool *lock, GLT_felock felock)
-{
+GLT_func_prefix void glt_felock_lock(GLT_bool *lock, GLT_felock felock) {
     *lock = myth_felock_lock(felock);
 }
 
-GLT_func_prefix void glt_felock_wait_lock(GLT_bool *lock, GLT_felock felock, int val)
-{
-    *lock = myth_felock_wait_lock(felock,val);
+GLT_func_prefix void glt_felock_wait_lock(GLT_bool *lock, GLT_felock felock, int val) {
+    *lock = myth_felock_wait_lock(felock, val);
 }
 
-GLT_func_prefix void glt_felock_unlock(GLT_bool *unlock, GLT_felock felock)
-{
+GLT_func_prefix void glt_felock_unlock(GLT_bool *unlock, GLT_felock felock) {
     *unlock = myth_felock_unlock(felock);
 }
 
-GLT_func_prefix void glt_felock_status(GLT_felock_status *status, GLT_felock felock)
-{
+GLT_func_prefix void glt_felock_status(GLT_felock_status *status, GLT_felock felock) {
     *status = myth_felock_status(felock);
 }
 
-GLT_func_prefix void glt_felock_set_unlock(GLT_felock felock, int val)
-{
-    myth_felock_set_unlock(felock,val);
+GLT_func_prefix void glt_felock_set_unlock(GLT_felock felock, int val) {
+    myth_felock_set_unlock(felock, val);
 }
 
 #endif
