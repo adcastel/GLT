3cf10d94f7e05831eb9da7abca7a22a81549ffc2
Adrian Castello <adcastel@uji.es>
2016-04-21 10:27:45 +0000

    glt_util.c from argobots cleaned

--- src/argobots/glt_util.c
+++ src/argobots/glt_util.c
@@ -5,16 +5,12 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix int glt_get_num_threads() 
-{
+GLT_func_prefix int glt_get_num_threads() {
     return main_team->num_xstreams;
 }
 
-GLT_func_prefix int glt_get_thread_num() 
-{
+GLT_func_prefix int glt_get_thread_num() {
     int rank;
     ABT_xstream_self_rank(&rank);
     return rank;
 }
-
-
