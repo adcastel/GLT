a4480bd0ff4889a0fb59caad8e4c381829618581
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:43 +0000

    glt_loop.c from qthreads cleaned

--- src/qthreads/glt_loop.c
+++ src/qthreads/glt_loop.c
@@ -11,8 +11,7 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix int glt_can_loop_functions()
-{
+GLT_func_prefix int glt_can_loop_functions() {
 #ifdef CORE
     return 0;
 #else
@@ -22,50 +21,41 @@ GLT_func_prefix int glt_can_loop_functions()
 
 #ifndef CORE
 
-GLT_func_prefix void glt_loop(const size_t start, const size_t end, 
-         const GLT_loop_f func, void * arg){
-    qt_loop(start,end,func,arg);
+GLT_func_prefix void glt_loop(const size_t start, const size_t end,
+        const GLT_loop_f func, void * arg) {
+    qt_loop(start, end, func, arg);
 }
 
-
-GLT_func_prefix void glt_loop_balance(const size_t start, const size_t end, 
-         const GLT_loop_f func, void * arg){
-    qt_loop_balance(start,end,func,arg);
+GLT_func_prefix void glt_loop_balance(const size_t start, const size_t end,
+        const GLT_loop_f func, void * arg) {
+    qt_loop_balance(start, end, func, arg);
 }
 
-
-GLT_func_prefix void glt_loopaccum_balance(const size_t start, const size_t end, 
-         size_t size, void *out, const GLT_loopr_f func, void * arg, GLT_accum_f acc){
-    qt_loopaccum_balance(start,end,size,out,func,arg,acc);
+GLT_func_prefix void glt_loopaccum_balance(const size_t start, const size_t end,
+        size_t size, void *out, const GLT_loopr_f func, void * arg, GLT_accum_f acc) {
+    qt_loopaccum_balance(start, end, size, out, func, arg, acc);
 }
-
-
 
 GLT_func_prefix void glt_loop_queue_create(GLT_loop_queue * loop, GLT_loop_queue_kind kind,
-        const size_t start, const size_t end, 
-        const size_t stride, const GLT_loop_f func, void * arg)
-{
-    loop = qt_loop_queue_create(kind,start,end,stride,func,arg);
+        const size_t start, const size_t end,
+        const size_t stride, const GLT_loop_f func, void * arg) {
+    loop = qt_loop_queue_create(kind, start, end, stride, func, arg);
 }
 
-GLT_func_prefix void glt_loop_queue_set_chunk(GLT_loop_queue * loop, size_t chunk)
-{
+GLT_func_prefix void glt_loop_queue_set_chunk(GLT_loop_queue * loop, size_t chunk) {
     qt_loop_queue_setchunk(loop, chunk);
 }
 
-GLT_func_prefix void glt_loop_queue_run(GLT_loop_queue * loop)
-{
+GLT_func_prefix void glt_loop_queue_run(GLT_loop_queue * loop) {
     qt_loop_queue_run(loop);
 }
 
-GLT_func_prefix void glt_loop_queue_run_there(GLT_loop_queue * loop, GLT_thread id)
-{
-    qt_loop_queue_run_there(loop,id);
+GLT_func_prefix void glt_loop_queue_run_there(GLT_loop_queue * loop, GLT_thread id) {
+    qt_loop_queue_run_there(loop, id);
 }
 
-GLT_func_prefix void glt_loop_queue_add_thread(GLT_loop_queue * loop, GLT_thread id)
-{
-    qt_loop_queue_addworker(loop,id);
+GLT_func_prefix void glt_loop_queue_add_thread(GLT_loop_queue * loop, GLT_thread id) {
+    qt_loop_queue_addworker(loop, id);
 }
 
 #endif
