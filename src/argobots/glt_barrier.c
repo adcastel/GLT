b43b3f754b130a7771d81681d1c1263cde36bbd7
Adrian Castello <adcastel@uji.es>
2016-04-21 10:27:43 +0000

    glt_barrier.c from argobots cleaned

--- src/argobots/glt_barrier.c
+++ src/argobots/glt_barrier.c
@@ -5,19 +5,14 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix void glt_barrier_create(int num_waiters, GLT_barrier *barrier)
-{
+GLT_func_prefix void glt_barrier_create(int num_waiters, GLT_barrier *barrier) {
     ABT_barrier_create(num_waiters, barrier);
 }
 
-GLT_func_prefix void glt_barrier_free(GLT_barrier *barrier)
-{
+GLT_func_prefix void glt_barrier_free(GLT_barrier *barrier) {
     ABT_barrier_free(barrier);
 }
 
-GLT_func_prefix void glt_barrier_wait(GLT_barrier *barrier)
-{
+GLT_func_prefix void glt_barrier_wait(GLT_barrier *barrier) {
     ABT_barrier_wait(*barrier);
 }
-
-
