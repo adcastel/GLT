70ec352a08bb156ac74360d17bfd8238727f0361
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:44 +0000

    glt_timer.c from qthreads cleaned

--- src/qthreads/glt_timer.c
+++ src/qthreads/glt_timer.c
@@ -7,35 +7,29 @@
 
 #include <sys/time.h>
 
-GLT_func_prefix double glt_get_wtime() 
-{
+GLT_func_prefix double glt_get_wtime() {
     struct timeval time;
-    gettimeofday(&time,NULL);
-    return (time.tv_sec * 1000000 + time.tv_usec)/1000000.0;
+    gettimeofday(&time, NULL);
+    return (time.tv_sec * 1000000 + time.tv_usec) / 1000000.0;
 }
 
-GLT_func_prefix void glt_timer_create(GLT_timer * timer) 
-{
+GLT_func_prefix void glt_timer_create(GLT_timer * timer) {
     *timer = qtimer_create();
 }
 
-GLT_func_prefix void glt_timer_free(GLT_timer * timer) 
-{
+GLT_func_prefix void glt_timer_free(GLT_timer * timer) {
     qtimer_destroy(*timer);
 }
 
-GLT_func_prefix void glt_timer_start(GLT_timer timer) 
-{
+GLT_func_prefix void glt_timer_start(GLT_timer timer) {
     qtimer_start(timer);
 }
 
-GLT_func_prefix void glt_timer_stop(GLT_timer timer) 
-{
+GLT_func_prefix void glt_timer_stop(GLT_timer timer) {
     qtimer_stop(timer);
 }
 
-GLT_func_prefix void glt_timer_get_secs(GLT_timer timer, double *secs) 
-{
+GLT_func_prefix void glt_timer_get_secs(GLT_timer timer, double *secs) {
     *secs = qtimer_secs(timer);
 }
 
