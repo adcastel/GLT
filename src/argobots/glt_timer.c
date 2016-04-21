b8cb49d0219832fc9d6787f892542418a22cc00b
Adrian Castello <adcastel@uji.es>
2016-04-21 10:27:45 +0000

    glt_timer.c from argobots cleaned

--- src/argobots/glt_timer.c
+++ src/argobots/glt_timer.c
@@ -5,33 +5,26 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix double glt_get_wtime() 
-{
+GLT_func_prefix double glt_get_wtime() {
     return ABT_get_wtime();
 }
 
-GLT_func_prefix void glt_timer_create(GLT_timer * timer) 
-{
+GLT_func_prefix void glt_timer_create(GLT_timer * timer) {
     ABT_timer_create(timer);
 }
 
-GLT_func_prefix void glt_timer_free(GLT_timer * timer) 
-{
+GLT_func_prefix void glt_timer_free(GLT_timer * timer) {
     ABT_timer_free(timer);
 }
 
-GLT_func_prefix void glt_timer_start(GLT_timer timer) 
-{
+GLT_func_prefix void glt_timer_start(GLT_timer timer) {
     ABT_timer_start(timer);
 }
 
-GLT_func_prefix void glt_timer_stop(GLT_timer timer) 
-{
+GLT_func_prefix void glt_timer_stop(GLT_timer timer) {
     ABT_timer_stop(timer);
 }
 
-GLT_func_prefix void glt_timer_get_secs(GLT_timer timer, double *secs) 
-{
-    ABT_timer_read(timer,secs);
+GLT_func_prefix void glt_timer_get_secs(GLT_timer timer, double *secs) {
+    ABT_timer_read(timer, secs);
 }
-
