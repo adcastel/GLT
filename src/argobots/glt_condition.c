21eb1eaf7617ac319fff20cc8d76a32066e0857b
Adrian Castello <adcastel@uji.es>
2016-04-21 10:27:44 +0000

    glt_condition.c from argobots cleaned

--- src/argobots/glt_condition.c
+++ src/argobots/glt_condition.c
@@ -5,27 +5,22 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix void glt_cond_create(GLT_cond *cond)
-{
-    ABT_cond_create (cond);
+GLT_func_prefix void glt_cond_create(GLT_cond *cond) {
+    ABT_cond_create(cond);
 }
 
-GLT_func_prefix void glt_cond_free(GLT_cond *cond)
-{
-    ABT_cond_free (cond);
+GLT_func_prefix void glt_cond_free(GLT_cond *cond) {
+    ABT_cond_free(cond);
 }
 
-GLT_func_prefix void glt_cond_signal(GLT_cond cond)
-{
-    ABT_cond_signal (cond);
+GLT_func_prefix void glt_cond_signal(GLT_cond cond) {
+    ABT_cond_signal(cond);
 }
 
-GLT_func_prefix void glt_cond_wait(GLT_cond cond, GLT_mutex mutex)
-{
-    ABT_cond_wait (cond, mutex);
+GLT_func_prefix void glt_cond_wait(GLT_cond cond, GLT_mutex mutex) {
+    ABT_cond_wait(cond, mutex);
 }
 
-GLT_func_prefix void glt_cond_broadcast(GLT_cond cond)
-{
-    ABT_cond_broadcast (cond);
+GLT_func_prefix void glt_cond_broadcast(GLT_cond cond) {
+    ABT_cond_broadcast(cond);
 }
