bdb8e24eb81cb2422f3f32e251e6180c50577393
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:42 +0000

    glt_tls.c from massivethreads cleaned

--- src/massivethreads/glt_tls.c
+++ src/massivethreads/glt_tls.c
@@ -10,8 +10,8 @@
 #else
 #define GLT_func_prefix static inline
 #endif
-GLT_func_prefix int glt_can_tls_functions()
-{
+
+GLT_func_prefix int glt_can_tls_functions() {
 #ifdef CORE
     return 0;
 #else
@@ -21,23 +21,19 @@ GLT_func_prefix int glt_can_tls_functions()
 
 #ifndef CORE
 
-GLT_func_prefix void glt_key_create (void(*destructor)(void *value), GLT_key *newkey)
-{
-    myth_key_create(newkey,destructor);
+GLT_func_prefix void glt_key_create(void(*destructor)(void *value), GLT_key *newkey) {
+    myth_key_create(newkey, destructor);
 }
 
-GLT_func_prefix void glt_key_free (GLT_key *key)
-{
-    myth_key_delete (*key);
+GLT_func_prefix void glt_key_free(GLT_key *key) {
+    myth_key_delete(*key);
 }
 
-GLT_func_prefix void glt_key_set (GLT_key key, void *value)
-{
-    myth_setspecific (key,value);
+GLT_func_prefix void glt_key_set(GLT_key key, void *value) {
+    myth_setspecific(key, value);
 }
 
-GLT_func_prefix void glt_key_get (GLT_key key, void **value)
-{
-    *value = myth_getspecific (key);
+GLT_func_prefix void glt_key_get(GLT_key key, void **value) {
+    *value = myth_getspecific(key);
 }
 #endif
