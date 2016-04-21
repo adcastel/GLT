ef81b1ce814fc1ab5fcb61b621aa6c32cd7dfe96
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:44 +0000

    glt_sinc.c from qthreads cleaned

--- src/qthreads/glt_sinc.c
+++ src/qthreads/glt_sinc.c
@@ -11,8 +11,7 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix int glt_can_sinc_functions()
-{
+GLT_func_prefix int glt_can_sinc_functions() {
 #ifdef CORE
     return 0;
 #else
@@ -23,40 +22,33 @@ GLT_func_prefix int glt_can_sinc_functions()
 #ifndef CORE
 
 GLT_func_prefix void glt_sinc_create(GLT_sinc *sinc, size_t sizeof_value,
-        const void * initial_value, GLT_sinc_op op, size_t expected)
-{
+        const void * initial_value, GLT_sinc_op op, size_t expected) {
     sinc = qt_sinc_create(sizeof_value, initial_value, op, expected);
 }
 
-GLT_func_prefix void glt_sinc_init(GLT_sinc *restrict sinc, size_t sizeof_value,
-        const void * initial_value, GLT_sinc_op op, size_t expected)
-{
+GLT_func_prefix void glt_sinc_init(GLT_sinc * restrict sinc, size_t sizeof_value,
+        const void * initial_value, GLT_sinc_op op, size_t expected) {
     qt_sinc_init(sinc, sizeof_value, initial_value, op, expected);
 }
 
-GLT_func_prefix void glt_sinc_destroy(GLT_sinc *sinc)
-{
+GLT_func_prefix void glt_sinc_destroy(GLT_sinc *sinc) {
     qt_sinc_destroy(sinc);
 }
 
-GLT_func_prefix void glt_sinc_fini(GLT_sinc * sinc)
-{
+GLT_func_prefix void glt_sinc_fini(GLT_sinc * sinc) {
     qt_sinc_fini(sinc);
 }
 
-GLT_func_prefix void glt_sinc_reset(GLT_sinc * sinc, size_t expect)
-{
-    qt_sinc_reset(sinc,expect);
+GLT_func_prefix void glt_sinc_reset(GLT_sinc * sinc, size_t expect) {
+    qt_sinc_reset(sinc, expect);
 }
 
-GLT_func_prefix void glt_sinc_submit(GLT_sinc * restrict sinc, const void * restrict value)
-{
-    qt_sinc_submit(sinc,value);
+GLT_func_prefix void glt_sinc_submit(GLT_sinc * restrict sinc, const void * restrict value) {
+    qt_sinc_submit(sinc, value);
 }
 
-GLT_func_prefix void glt_sinc_wait(GLT_sinc * restrict sinc,  void * restrict target)
-{
-    qt_sinc_wait(sinc,target);
+GLT_func_prefix void glt_sinc_wait(GLT_sinc * restrict sinc, void * restrict target) {
+    qt_sinc_wait(sinc, target);
 }
 
 #endif
