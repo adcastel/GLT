35e0f5f082bdbedf503547311f75a4757291391d
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:43 +0000

    glt_barrier.c from qthreads cleaned

--- src/qthreads/glt_barrier.c
+++ src/qthreads/glt_barrier.c
@@ -4,18 +4,16 @@
 #else
 #define GLT_func_prefix static inline
 #endif
-GLT_func_prefix void glt_barrier_create(int num_waiters, GLT_barrier *barrier)
-{
+
+GLT_func_prefix void glt_barrier_create(int num_waiters, GLT_barrier *barrier) {
     barrier = qt_barrier_create(num_waiters, UPLOCK);
 }
 
-GLT_func_prefix void glt_barrier_free(GLT_barrier *barrier)
-{
+GLT_func_prefix void glt_barrier_free(GLT_barrier *barrier) {
     qt_barrier_destroy(barrier);
 }
 
-GLT_func_prefix void glt_barrier_wait(GLT_barrier *barrier)
-{
+GLT_func_prefix void glt_barrier_wait(GLT_barrier *barrier) {
     qt_barrier_enter(barrier);
 }
 
