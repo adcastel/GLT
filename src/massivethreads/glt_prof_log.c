fffb82d0e4ca81127e6eb6ec5fcc897bda2fa511
Adrian Castello <adcastel@uji.es>
2016-04-21 10:28:42 +0000

    glt_prof_log.c from massivethreads cleaned

--- src/massivethreads/glt_prof_log.c
+++ src/massivethreads/glt_prof_log.c
@@ -11,8 +11,7 @@
 #define GLT_func_prefix static inline
 #endif
 
-GLT_func_prefix int glt_can_log_functions()
-{
+GLT_func_prefix int glt_can_log_functions() {
 #ifdef CORE
     return 0;
 #else
@@ -21,33 +20,29 @@ GLT_func_prefix int glt_can_log_functions()
 }
 
 #ifndef CORE
-GLT_func_prefix void glt_log_start()
-{
+
+GLT_func_prefix void glt_log_start() {
     myth_log_start();
 }
 
-GLT_func_prefix void glt_log_pause()
-{
+GLT_func_prefix void glt_log_pause() {
     myth_log_pause();
 }
 
-GLT_func_prefix void glt_log_flush()
-{
+GLT_func_prefix void glt_log_flush() {
     myth_log_flush();
 }
-GLT_func_prefix void glt_log_reset()
-{
+
+GLT_func_prefix void glt_log_reset() {
     myth_log_reset();
 }
 
-GLT_func_prefix void glt_log_annotate_ult(GLT_ult ult, char *name)
-{
-    myth_log_annotate_thread(ult,name);
+GLT_func_prefix void glt_log_annotate_ult(GLT_ult ult, char *name) {
+    myth_log_annotate_thread(ult, name);
 }
 #endif
 
-GLT_func_prefix int glt_can_prof_functions()
-{
+GLT_func_prefix int glt_can_prof_functions() {
 #ifdef CORE
     return 0;
 #else
@@ -56,13 +51,12 @@ GLT_func_prefix int glt_can_prof_functions()
 }
 
 #ifndef CORE
-GLT_func_prefix void glt_sched_prof_start()
-{
+
+GLT_func_prefix void glt_sched_prof_start() {
     myth_sched_prof_start();
 }
 
-GLT_func_prefix void glt_sched_prof_pause()
-{
+GLT_func_prefix void glt_sched_prof_pause() {
     myth_sched_prof_pause();
 }
 #endif
