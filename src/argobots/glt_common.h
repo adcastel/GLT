a19804ea681bc35278f6168e17660404d2dda56a
Adrian Castello <adcastel@uji.es>
2016-04-25 14:07:08 +0000

    glt_common.h cleaned

--- src/argobots/glt_common.h
+++ src/argobots/glt_common.h
@@ -5,7 +5,7 @@
  *      Antonio J. Peña (antonio.pena@bsc.es) at Barcelona Supercomputing Center
  *      Rafael Mayo Gual and Enrique S. Quintana-Ortí ({mayo,quintana}@uji.es)
  *      Sangmin Seo and Pavan Balaji ({sseo,balaji}@anl.gov) at Argonne National Laboratory
-*/
+ */
 
 /* Argobots implementation object file*/
 
@@ -34,26 +34,50 @@
 #define GLT_sched_config ABT_sched_config
 #define GLT_sched_config_var ABT_sched_config_var
 #define GLT_sched_config_var_end ABT_sched_config_var_end
-#define GLT_sched_def ABT_sched_def
-#define GLT_sched_predef ABT_sched_predef
 
 #define glt_scheduler_config_create  ABT_sched_config_create
 #define glt_scheduler_config_read  ABT_sched_config_read 
 
 #define GLT_UNIT_NULL ABT_UNIT_NULL
+#define GLT_TRUE    1
+#define GLT_FALSE   0
 
-#define GLT_TRUE ABT_TRUE
-#define GLT_FALSE ABT_FALSE
-
-#define GLT_POOL_FIFO ABT_POOL_FIFO
-#define GLT_POOL_ACCESS_MPMC ABT_POOL_ACCESS_MPMC
+/* ABT_sched_type*/
 #define GLT_SCHED_TYPE_ULT ABT_SCHED_TYPE_ULT
+#define GLT_SCHED_TYPE_TASK ABT_SCHED_TYPE_TASK
 
-typedef enum {
-    GLT_SCHED_CONFIG_INT = 0, /* Parameter of type int */
-    GLT_SCHED_CONFIG_DOUBLE = 1, /* Parameter of type double */
-    GLT_SCHED_CONFIG_PTR = 2, /* Parameter of type pointer */
-} GLT_sched_config_type;
+
+/* ABT_sched_def*/
+#define GLT_sched_type ABT_sched_type /* ULT or tasklet */
+#define GLT_sched_init_fn ABT_sched_init_fn
+#define GLT_sched_run_fn ABT_sched_run_fn
+#define GLT_sched_free_fn ABT_sched_free_fn
+#define GLT_sched_get_migr_pool_fn ABT_sched_get_migr_pool_fn
+#define GLT_sched_def ABT_sched_def
+
+/* ABT_sched_predef*/
+#define GLT_SCHED_DEFAULT ABT_SCHED_DEFAULT/* Default scheduler */
+#define GLT_SCHED_BASIC ABT_SCHED_BASIC/* Basic scheduler */
+#define GLT_SCHED_PRIO ABT_SCHED_PRIO/* Priority scheduler */
+#define GLT_sched_predef ABT_sched_predef
+
+/* ABT_pool_kind*/
+#define GLT_POOL_FIFO ABT_POOL_FIFO
+#define GLT_pool_kind ABT_pool_kind
+
+/* ABT_pool_access*/
+#define GLT_POOL_ACCESS_PRIV ABT_POOL_ACCESS_PRIV/* Used by only one ES */
+#define GLT_POOL_ACCESS_SPSC ABT_POOL_ACCESS_SPSC/* Producers on ES1, consumers on ES2 */
+#define GLT_POOL_ACCESS_MPSC ABT_POOL_ACCESS_MPSC/* Producers on any ES, consumers on the same ES */
+#define GLT_POOL_ACCESS_SPMC ABT_POOL_ACCESS_SPMC/* Producers on the same ES, consumers on any ES */
+#define GLT_POOL_ACCESS_MPMC ABT_POOL_ACCESS_MPMC/* Producers on any ES, consumers on any ES */
+#define GLT_pool_access ABT_pool_access
+
+/* ABT_sched_config_type*/
+#define GLT_SCHED_CONFIG_INT  ABT_SCHED_CONFIG_INT/* Parameter of type int */
+#define GLT_SCHED_CONFIG_DOUBLE ABT_SCHED_CONFIG_DOUBLE /* Parameter of type double */
+#define GLT_SCHED_CONFIG_PTR ABT_SCHED_CONFIG_PTR /* Parameter of type pointer */
+#define GLT_sched_config_type ABT_sched_config_type
 
 //Extended variables
 #ifndef CORE
@@ -76,13 +100,13 @@ typedef enum {
 
 
 //MASSIVETHREAD
-#define GLT_workunit_f void *
-#define GLT_workunit_o void *
-#define GLT_felock void *
-#define GLT_felock_status int
-#define GLT_pickle void *
-#define GLT_wsapi_decide_f void *
-#define GLT_wsapi_steal_f void *
+typedef void* GLT_workunit_f;
+typedef void* GLT_workunit_o;
+typedef void* GLT_felock;
+typedef int GLT_felock_status;
+typedef void* GLT_pickle;
+typedef void* GLT_wsapi_decide_f;
+typedef void* GLT_wsapi_steal_f;
 //Shared
 #define GLT_key ABT_key
 
@@ -97,39 +121,32 @@ typedef enum introspective_state {
     NOT_SUPPORTED
 } introspective_state;
 
-typedef struct mapinfo_s {
-    int not_supported;
-} mapinfo_s_t;
 
-typedef struct dynmapinfo_s {
-    int not_supported;
-} dynmapinfo_s_t;
-
-#define GLT_syncvar void *
-#define GLT_aligned void *
-#define GLT_memory_state void *
-#define GLT_sinc_op void *
-#define GLT_sinc void *
-#define GLT_loop_f void *
-#define GLT_loopr_f void *
-#define GLT_loop_step_f void *
-#define GLT_accum_f void *
-#define GLT_loop_queue void *
-#define GLT_loop_queue_kind void *
-#define GLT_ds_pool void *
-#define GLT_ds_array void *
-#define GLT_ds_array_distribution void *
-#define GLT_ds_loop_f void *
-#define GLT_ds_loopr_f void *
-#define GLT_ds_cloop_f void *
-#define GLT_ds_lfqueue void *
-#define GLT_ds_dqueue void *
-#define GLT_ds_dictionary void *
-#define GLT_ds_dictionary_key_equals void *
-#define GLT_ds_dictionary_hash void *
-#define GLT_ds_dictionary_cleanup void *
-#define GLT_ds_dictionary_it void *
-#define GLT_subthread void *
+typedef void* GLT_syncvar;
+typedef void* GLT_aligned;
+typedef void* GLT_memory_state;
+typedef void* GLT_sinc_op;
+typedef void* GLT_sinc;
+typedef void* GLT_loop_f;
+typedef void* GLT_loopr_f;
+typedef void* GLT_loop_step_f;
+typedef void* GLT_accum_f;
+typedef void* GLT_loop_queue;
+typedef void* GLT_loop_queue_kind;
+typedef void* GLT_ds_pool;
+typedef void* GLT_ds_array;
+typedef void* GLT_ds_array_distribution;
+typedef void* GLT_ds_loop_f;
+typedef void* GLT_ds_loopr_f;
+typedef void* GLT_ds_cloop_f;
+typedef void* GLT_ds_lfqueue;
+typedef void* GLT_ds_dqueue;
+typedef void* GLT_ds_dictionary;
+typedef void* GLT_ds_dictionary_key_equals;
+typedef void* GLT_ds_dictionary_hash;
+typedef void* GLT_ds_dictionary_cleanup;
+typedef void* GLT_ds_dictionary_it;
+typedef void* GLT_subthread;
 
 #define glt_ult_creation_precond qthread_fork_precond
 #define glt_ult_creation_precond_to qthread_fork_precond_to
