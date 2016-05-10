94e9be3d5c34d6faa6adf1ee7a9dd18e5f492954
Adrian Castello <adcastel@uji.es>
2016-05-10 12:12:48 +0000

    glt_wu.c pthread file added

--- src/pthreads/glt_wu.c
+++ src/pthreads/glt_wu.c
@@ -21,35 +21,51 @@ GLT_func_prefix GLT_tasklet * glt_tasklet_malloc(int number_of_tasklets) {
 }
 
 GLT_func_prefix void glt_ult_creation(void(*thread_func)(void *), void *arg, GLT_ult *new_ult) {
-    *new_ult = myth_create((void *) thread_func, arg);
+    pthread_create(new_ult, NULL,(void *) thread_func, arg);
+    main_team->num_workers++;
 }
 
 GLT_func_prefix void glt_ult_creation_to(void(*thread_func)(void *), void *arg, GLT_ult *new_ult, int dest) {
-    glt_ult_creation(thread_func, arg, new_ult);
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    dest=dest%main_team->max_workers;
+    CPU_SET(dest, &cpuset);
+    pthread_create(new_ult, NULL,(void *) thread_func, arg);
+    pthread_setaffinity_np(*new_ult, sizeof (cpu_set_t), &cpuset);
+    main_team->num_workers++;
 }
 
 GLT_func_prefix void glt_tasklet_creation(void(*thread_func)(void *), void *arg, GLT_tasklet *new_ult) {
-    *new_ult = myth_create((void *) thread_func, arg);
+    pthread_create(new_ult, NULL,(void *) thread_func, arg);
+    main_team->num_workers++;
 }
 
 GLT_func_prefix void glt_tasklet_creation_to(void(*thread_func)(void *), void *arg, GLT_tasklet *new_ult, int dest) {
-    glt_ult_creation(thread_func, arg, new_ult);
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    dest=dest%main_team->max_workers;
+    CPU_SET(dest, &cpuset);
+    pthread_create(new_ult, NULL,(void *) thread_func, arg);
+    pthread_setaffinity_np(*new_ult, sizeof (cpu_set_t), &cpuset);
+    main_team->num_workers++;
 }
 
 GLT_func_prefix void glt_yield() {
-    myth_yield(0);
+    pthread_yield();
 }
 
 GLT_func_prefix void glt_yield_to(GLT_ult ult) {
-    myth_yield(0);
+    pthread_yield();
 }
 
 GLT_func_prefix void glt_ult_join(GLT_ult *ult) {
-    myth_join(*ult, NULL);
+    pthread_join(*ult, NULL);
+    main_team->num_workers--;
 }
 
 GLT_func_prefix void glt_tasklet_join(GLT_tasklet *tasklet) {
-    myth_join(*tasklet, NULL);
+    pthread_join(*tasklet, NULL);
+    main_team->num_workers--;
 }
 
 GLT_func_prefix void glt_ult_get_id(GLT_ult_id * id, GLT_ult ult) {
@@ -58,7 +74,7 @@ GLT_func_prefix void glt_ult_get_id(GLT_ult_id * id, GLT_ult ult) {
 }
 
 GLT_func_prefix void glt_workunit_get_thread_id(GLT_thread_id *id) {
-    *id = myth_get_worker_num();
+    *id = pthread_getthreadid_np();
 }
 
 GLT_func_prefix void glt_ult_migrate_self_to(GLT_thread_id id) {
@@ -66,63 +82,25 @@ GLT_func_prefix void glt_ult_migrate_self_to(GLT_thread_id id) {
 }
 
 GLT_func_prefix void glt_tasklet_self(GLT_tasklet *tasklet) {
-    *tasklet = myth_self();
+    *tasklet = pthread_self();
 }
 
 GLT_func_prefix void glt_ult_self(GLT_ult *ult) {
-    *ult = myth_self();
+    *ult = pthread_self();
 }
 
 GLT_func_prefix void glt_ult_exit() {
     void *ret;
-    myth_exit(ret);
+    pthread_exit(ret);
+    main_team->num_workers--;
 }
 
 GLT_func_prefix void glt_ult_cancel(GLT_ult ult) {
-    myth_cancel(ult);
+    pthread_cancel(ult);
+    main_team->num_workers--;
 }
 
 GLT_func_prefix void glt_tasklet_cancel(GLT_tasklet tasklet) {
-    myth_cancel(tasklet);
+    pthread_cancel(tasklet);
+    main_team->num_workers--;
 }
-
-GLT_func_prefix int can_extended_workunits() {
-#ifdef CORE
-    return 0;
-#else
-    return 1;
-#endif
-}
-
-#ifndef CORE
-
-GLT_func_prefix void glt_wu_create_ex(GLT_ult * ult, GLT_workunit_f func, void * arg,
-        GLT_workunit_o opt) {
-    *ult = myth_create_ex(func, arg, opt);
-}
-
-GLT_func_prefix void glt_yield2() {
-    myth_yield2();
-}
-
-GLT_func_prefix void glt_wu_detach(GLT_ult ult) {
-    myth_detach(ult);
-}
-
-GLT_func_prefix void glt_wu_set_cancel_state(int state, int *oldstate) {
-    myth_setcancelstate(state, oldstate);
-}
-
-GLT_func_prefix void glt_wu_set_cancel_type(int type, int *oldtype) {
-    myth_setcanceltype(type, oldtype);
-}
-
-GLT_func_prefix void glt_wu_test_cancel() {
-    myth_testcancel();
-}
-
-GLT_func_prefix void glt_wu_set_def_stack_size(size_t newsize) {
-    myth_set_def_stack_size(newsize);
-}
-
-#endif
