9dbeabdd4a7fe7b06d9fdbea49ed596cdd039ea3
Adrian Castello <adcastel@uji.es>
2016-05-12 11:38:24 +0000

    qthreads GLT version updated

--- src/qthreads/glt_common.h
+++ src/qthreads/glt_common.h
@@ -169,7 +169,7 @@ typedef struct glt_team {
 } glt_team_t;
 
 
-#define GLT_VERSION "You are using GLT 0.1v over Qthreads (adcastel@uji.es)"
+#define GLT_VERSION "You are using GLT 1.0v over Qthreads (adcastel@uji.es)"
 #define GLT_ERROR_ARG printf("Error: This feature is only supported by Argobots implementation and you are using Qthreads. Please use the query functions\n")
 #define GLT_ERROR_MYTH printf("Error: This feature is only supported by MassiveThredashreads implementation and you are using Qthreads. Please use the query functions\n")
 #define GLT_LIB_ERROR printf("Error: This feature is in the API but it is not included in the Library\n")
