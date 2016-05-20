a4e8978f2108df0b556982d18bc333cb1906e6f2
Adrian Castello <adcastel@uji.es>
2016-05-20 14:16:31 +0000

    pthread API suppported by MTH

---
+++ src/massivethreads/glt_pthreads.c
@@ -0,0 +1,170 @@
+/* See COPYRIGHT in top-level directory. */
+/*
+ * Pthread-like API file.
+ */
+
+#include<glt_pthreads.h>
+
+void glt_pthread_start() {
+    char buff[10];
+    int num_threads = get_nprocs();
+    int num_pools = 1;
+    
+    sprintf(buff, "%d", num_threads);
+    setenv("GLT_NUM_THREADS", buff, 1);
+    int argc = 0;
+    char * argv;
+
+    glt_init(argc, &argv);
+}
+
+void glt_pthread_end() {
+    glt_finalize();
+}
+
+int gsched_yield(void) {
+    glt_yield();
+    return 0;
+}
+
+pthread_t gpthread_self(void) {
+    GLT_ult ult;
+    glt_ult_self(&ult);
+    return (pthread_t) ult;
+}
+
+int gpthread_create(pthread_t *pth, const pthread_attr_t * attr, void *(*func)(void*), void *args) {
+    GLT_ult ult;
+    glt_ult_create((void *) func, args, &ult);
+    *pth = (pthread_t) ult;
+    return 0;
+}
+
+int gpthread_join(pthread_t th, void**ret) {
+    glt_ult_join((GLT_ult *) & th);
+    return 0;
+}
+
+void gpthread_exit(void *ret) {
+    glt_ult_exit();
+    //To avoid warning, this code is unreachable
+    while (1);
+}
+
+int gpthread_detach(pthread_t th) {
+    glt_ult_cancel((GLT_ult) th);
+    return 0;
+}
+
+int gpthread_setcancelstate(int state, int *oldstate) {
+    return 0;
+}
+
+int gpthread_setcanceltype(int type, int *oldtype) {
+    return 0;
+}
+
+int gpthread_cancel(pthread_t th) {
+    glt_ult_cancel((GLT_ult) th);
+    return 0;
+}
+
+void gpthread_testcancel(void) {
+
+}
+
+int gpthread_key_create(pthread_key_t *key, void (*destructor) (void *)) {
+    glt_key_create(destructor, (GLT_key *) & key);
+    return 0;
+}
+
+int gpthread_key_delete(pthread_key_t key) {
+    glt_key_free((GLT_key *) & key);
+    return 0;
+}
+
+void *gpthread_getspecific(pthread_key_t key) {
+    void * value;
+    glt_key_get(key, &value);
+    return value;
+}
+
+int gpthread_setspecific(pthread_key_t key, const void *ptr) {
+    glt_key_set(key, (void *) ptr);
+    return 0;
+}
+
+//TODO:these functions are too linux-dependent, need to make more portable
+
+int gpthread_barrier_init(pthread_barrier_t *barrier, const pthread_barrierattr_t *attr, unsigned int count) {
+    glt_barrier_create(count, (GLT_barrier *) &barrier);
+    return 0;
+}
+
+/* Destroy a previously dynamically initialized barrier BARRIER.  */
+
+int gpthread_barrier_destroy(pthread_barrier_t *barrier) {
+    glt_barrier_free((GLT_barrier *) &barrier);
+    return 0;
+}
+
+/* Wait on barrier BARRIER.  */
+
+int gpthread_barrier_wait(pthread_barrier_t *barrier) {
+    glt_barrier_wait((GLT_barrier *) &barrier);
+    return 0;
+}
+
+int gpthread_mutex_init(pthread_mutex_t *mutex,
+        const pthread_mutexattr_t *attr) {
+    glt_mutex_create((GLT_mutex *) &mutex);
+    return 0;
+}
+
+int gpthread_mutex_destroy(pthread_mutex_t *mutex) {
+    glt_mutex_free((GLT_mutex *) &mutex);
+    return 0;
+}
+
+int gpthread_mutex_trylock(pthread_mutex_t *mutex) {
+    GLT_bool try = GLT_FALSE;
+    glt_mutex_trylock(&try, (GLT_mutex) mutex);
+    return try;
+}
+
+int gpthread_mutex_lock(pthread_mutex_t *mutex) {
+    glt_mutex_lock((GLT_mutex) mutex);
+    return 0;
+}
+
+int gpthread_mutex_unlock(pthread_mutex_t *mutex) {
+    glt_mutex_unlock((GLT_mutex) mutex);
+    return 0;
+}
+
+int gpthread_cond_init(pthread_cond_t * c,
+        const pthread_condattr_t *cond_attr) {
+    glt_cond_create((GLT_cond *) &c);
+    return 0;
+}
+
+int gpthread_cond_destroy(pthread_cond_t *c) {
+    glt_cond_free((GLT_cond *) &c);
+    return 0;
+}
+
+int gpthread_cond_signal(pthread_cond_t *c) {
+    glt_cond_signal((GLT_cond) c);
+    return 0;
+}
+
+int gpthread_cond_broadcast(pthread_cond_t *c) {
+    glt_cond_broadcast((GLT_cond) c);
+    return 0;
+}
+
+int gpthread_cond_wait(pthread_cond_t * c,
+        pthread_mutex_t *mutex) {
+    glt_cond_wait((GLT_cond) c, (GLT_mutex) mutex);
+    return 0;
+}
---
+++ src/massivethreads/glt_pthreads.h
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   glt_pthreads.h
+ * Author: adcastel
+ *
+ * Created on 16 de mayo de 2016, 17:15
+ */
+
+#ifndef GLT_PTHREADS_H
+#define GLT_PTHREADS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <assert.h>
+#include <dlfcn.h>
+#include <fast_glt.h>
+#include <pthread.h>
+//#include <myth.h>
+
+    void __attribute__((constructor)) glt_pthread_start(void);
+    void __attribute__((destructor)) glt_pthread_end(void);
+
+    int gpthread_create(pthread_t *pth, const pthread_attr_t * attr, void *(*func)(void*), void *args);
+    int gpthread_join(pthread_t th, void**ret);
+    int gsched_yield(void);
+    pthread_t gpthread_self(void);
+    void gpthread_exit(void *ret);
+    int gpthread_detach(pthread_t th);
+    int gpthread_setcancelstate(int state, int *oldstate);
+    int gpthread_setcanceltype(int type, int *oldtype);
+    int gpthread_cancel(pthread_t th);
+    void gpthread_testcancel(void);
+    int gpthread_key_create(pthread_key_t *key, void (*destructor) (void *));
+    int gpthread_key_delete(pthread_key_t key);
+    void *gpthread_getspecific(pthread_key_t key);
+    int gpthread_setspecific(pthread_key_t key, const void *ptr);
+    int gpthread_barrier_init(pthread_barrier_t *barrier,
+            const pthread_barrierattr_t *attr, unsigned int count);
+    int gpthread_barrier_destroy(pthread_barrier_t *barrier);
+    int gpthread_barrier_wait(pthread_barrier_t *barrier);
+    int gpthread_mutex_init(pthread_mutex_t *mutex,
+            const pthread_mutexattr_t *attr);
+    int gpthread_mutex_destroy(pthread_mutex_t *mutex);
+    int gpthread_mutex_trylock(pthread_mutex_t *mutex);
+    int gpthread_mutex_lock(pthread_mutex_t *mutex);
+    int gpthread_mutex_unlock(pthread_mutex_t *mutex);
+    int gpthread_cond_init(pthread_cond_t * c,
+            const pthread_condattr_t *cond_attr);
+    int gpthread_cond_destroy(pthread_cond_t *c);
+    int gpthread_cond_signal(pthread_cond_t *c);
+    int gpthread_cond_broadcast(pthread_cond_t *c);
+    int gpthread_cond_wait(pthread_cond_t * c,
+            pthread_mutex_t *mutex);
+
+#define pthread_create gpthread_create
+#define pthread_join gpthread_join
+#define sched_yield gsched_yield 
+#define pthread_self gpthread_self
+#define pthread_exit gpthread_exit
+#define pthread_detach gpthread_detach
+#define pthread_setcancelstate gpthread_setcancelstate
+#define pthread_setcanceltype gpthread_setcanceltype
+#define pthread_cancel gpthread_cancel
+#define pthread_testcancel gpthread_testcancel
+#define pthread_key_create gpthread_key_create
+#define pthread_key_delete gpthread_key_delete
+#define pthread_getspecific gpthread_getspecific
+#define pthread_setspecific gpthread_setspecific 
+#define pthread_barrier_init gpthread_barrier_init 
+#define pthread_barrier_destroy gpthread_barrier_destroy
+#define pthread_barrier_wait gpthread_barrier_wait
+#define pthread_mutex_init gpthread_mutex_init
+#define pthread_mutex_destroy gpthread_mutex_destroy
+#define pthread_mutex_trylock gpthread_mutex_trylock
+#define pthread_mutex_lock gpthread_mutex_lock
+#define pthread_mutex_unlock gpthread_mutex_unlock
+#define pthread_cond_init gpthread_cond_init
+#define pthread_cond_destroy gpthread_cond_destroy
+#define pthread_cond_signal gpthread_cond_signal
+#define pthread_cond_broadcast gpthread_cond_broadcast
+#define pthread_cond_wait gpthread_cond_wait 
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GLT_PTHREADS_H */
+
