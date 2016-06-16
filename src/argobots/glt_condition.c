4558926b68922122df19e0e767b3861f053cbc7a
Adrian Castello <adcastel@uji.es>
2016-06-16 14:20:21 +0000

    checks added to glt_condition.c file

--- src/argobots/glt_condition.c
+++ src/argobots/glt_condition.c
@@ -11,21 +11,21 @@
 #endif
 
 GLT_func_prefix void glt_cond_create(GLT_cond *cond) {
-    ABT_cond_create(cond);
+    CHECK(ABT_cond_create(cond),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_cond_free(GLT_cond *cond) {
-    ABT_cond_free(cond);
+    CHECK(ABT_cond_free(cond),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_cond_signal(GLT_cond cond) {
-    ABT_cond_signal(cond);
+    CHECK(ABT_cond_signal(cond),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_cond_wait(GLT_cond cond, GLT_mutex mutex) {
-    ABT_cond_wait(cond, mutex);
+    CHECK(ABT_cond_wait(cond, mutex),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_cond_broadcast(GLT_cond cond) {
-    ABT_cond_broadcast(cond);
+    CHECK(ABT_cond_broadcast(cond),ABT_SUCCESS);
 }
