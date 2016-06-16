08d634261a4e1be73513788ace29d034380c6814
Adrian Castello <adcastel@uji.es>
2016-06-16 14:20:23 +0000

    checks added to glt_thread.c file

--- src/argobots/glt_thread.c
+++ src/argobots/glt_thread.c
@@ -25,7 +25,7 @@ GLT_func_prefix int glt_can_manage_threads() {
 GLT_func_prefix void glt_thread_create(GLT_sched sched, GLT_thread *newthread) {
 
     if (main_team->num_xstreams < main_team->max_elem) {
-        ABT_xstream_create(sched, newthread);
+        CHECK(ABT_xstream_create(sched, newthread),ABT_SUCCESS);
         main_team->team[main_team->num_xstreams] = newthread;
         main_team->num_xstreams++;
                 printf("Creado nuevo thread y con esta llevamos %d\n",main_team->num_xstreams);
@@ -38,7 +38,7 @@ GLT_func_prefix void glt_thread_create(GLT_sched sched, GLT_thread *newthread) {
 GLT_func_prefix void glt_thread_create_basic(GLT_sched_predef predef, int num_pools,
         GLT_pool *pools, GLT_sched_config config, GLT_thread *newthread) {
     if (main_team->num_xstreams < main_team->max_elem) {
-        ABT_xstream_create_basic(predef, num_pools, pools, config, newthread);
+        CHECK(ABT_xstream_create_basic(predef, num_pools, pools, config, newthread),ABT_SUCCESS);
         main_team->team[main_team->num_xstreams] = newthread;
         main_team->num_xstreams++;
                         printf("Creado nuevo basic thread y con esta llevamos %d\n",main_team->num_xstreams);
@@ -49,76 +49,76 @@ GLT_func_prefix void glt_thread_create_basic(GLT_sched_predef predef, int num_po
 }
 
 GLT_func_prefix void glt_thread_start(GLT_thread thread) {
-    ABT_xstream_start(thread);
+    CHECK(ABT_xstream_start(thread),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_free(GLT_thread *thread) {
-    ABT_xstream_free(thread);
+    CHECK(ABT_xstream_free(thread),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_join(GLT_thread thread) {
-    ABT_xstream_join(thread);
+    CHECK(ABT_xstream_join(thread),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_exit() {
-    ABT_xstream_exit();
+    CHECK(ABT_xstream_exit(),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_cancel(GLT_thread *thread) {
-    ABT_xstream_cancel(thread);
+    CHECK(ABT_xstream_cancel(thread),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_self(GLT_thread *thread) {
-    ABT_xstream_self(thread);
+    CHECK(ABT_xstream_self(thread),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_self_rank(int *rank) {
-    ABT_xstream_self_rank(rank);
+    CHECK(ABT_xstream_self_rank(rank),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_get_rank(GLT_thread thread, int *rank) {
-    ABT_xstream_get_rank(thread, rank);
+    CHECK(ABT_xstream_get_rank(thread, rank),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_set_main_sched(GLT_thread thread, GLT_sched sched) {
-    ABT_xstream_set_main_sched(thread, sched);
+    CHECK(ABT_xstream_set_main_sched(thread, sched),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_set_main_sched_basic(GLT_thread thread,
         GLT_sched_predef predef, int num_pools, GLT_pool *pools) {
-    ABT_xstream_set_main_sched_basic(thread, predef, num_pools, pools);
+    CHECK(ABT_xstream_set_main_sched_basic(thread, predef, num_pools, pools),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_get_main_sched(GLT_thread thread, GLT_sched *sched) {
-    ABT_xstream_get_main_sched(thread, sched);
+    CHECK(ABT_xstream_get_main_sched(thread, sched),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_get_main_pools(GLT_thread thread, int max_pools,
         GLT_pool *pools) {
-    ABT_xstream_get_main_pools(thread, max_pools, pools);
+    CHECK(ABT_xstream_get_main_pools(thread, max_pools, pools),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_get_state(GLT_thread thread, GLT_thread_state *state) {
-    ABT_xstream_get_state(thread, state);
+    CHECK(ABT_xstream_get_state(thread, state),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_equal(GLT_thread thread1, GLT_thread thread2, GLT_bool *result) {
-    ABT_xstream_equal(thread1, thread2, result);
+    CHECK(ABT_xstream_equal(thread1, thread2, result),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_get_num(int *num_xstreams) {
-    ABT_xstream_get_num(num_xstreams);
+    CHECK(ABT_xstream_get_num(num_xstreams),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_is_primary(GLT_thread thread, GLT_bool *flag) {
-    ABT_xstream_is_primary(thread, flag);
+    CHECK(ABT_xstream_is_primary(thread, flag),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_run_unit(GLT_unit unit, GLT_pool pool) {
-    ABT_xstream_run_unit(unit, pool);
+    CHECK(ABT_xstream_run_unit(unit, pool),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_thread_check_events(GLT_sched sched) {
-    ABT_xstream_check_events(sched);
+    CHECK(ABT_xstream_check_events(sched),ABT_SUCCESS);
 }
 #endif
