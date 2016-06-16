e1b5557035755de6f4aea6e60b6de2b8d5ecc70f
Adrian Castello <adcastel@uji.es>
2016-06-16 14:20:22 +0000

    checks added to glt_event.c file

--- src/argobots/glt_event.c
+++ src/argobots/glt_event.c
@@ -22,17 +22,17 @@ GLT_func_prefix int glt_can_event_functions() {
 
 #ifndef CORE
 GLT_func_prefix void glt_get_error_str(int errno, char *str, size_t *len) {
-    ABT_error_get_str(errno, str, len);
+    CHECK(ABT_error_get_str(errno, str, len),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_event_add_callbac(GLT_event_kind event, GLT_event_cb_fn ask_cb,
         void *ask_user_arg, GLT_event_cb_fn act_cb, void *act_user_arg,
         int *cb_id) {
-    ABT_event_add_callback(event, ask_cb, ask_user_arg, act_cb,
-            act_user_arg, cb_id);
+    CHECK(ABT_event_add_callback(event, ask_cb, ask_user_arg, act_cb,
+            act_user_arg, cb_id),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_event_del_callback(GLT_event_kind event, int cb_id) {
-    ABT_event_del_callback(event, cb_id);
+    CHECK(ABT_event_del_callback(event, cb_id),ABT_SUCCESS);
 }
 #endif
