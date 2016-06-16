d68b4f7c72eecd22b419b41ddbb86e396cae776e
Adrian Castello <adcastel@uji.es>
2016-06-16 14:20:23 +0000

    checks added to glt_timer.c file

--- src/argobots/glt_timer.c
+++ src/argobots/glt_timer.c
@@ -15,21 +15,21 @@ GLT_func_prefix double glt_get_wtime() {
 }
 
 GLT_func_prefix void glt_timer_create(GLT_timer * timer) {
-    ABT_timer_create(timer);
+    CHECK(ABT_timer_create(timer),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_timer_free(GLT_timer * timer) {
-    ABT_timer_free(timer);
+    CHECK(ABT_timer_free(timer),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_timer_start(GLT_timer timer) {
-    ABT_timer_start(timer);
+    CHECK(ABT_timer_start(timer),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_timer_stop(GLT_timer timer) {
-    ABT_timer_stop(timer);
+    CHECK(ABT_timer_stop(timer),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_timer_get_secs(GLT_timer timer, double *secs) {
-    ABT_timer_read(timer, secs);
+    CHECK(ABT_timer_read(timer, secs),ABT_SUCCESS);
 }
