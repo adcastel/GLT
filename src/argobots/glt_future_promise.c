f028e17d02284452c8461bc23f6fd5426a2dc899
Adrian Castello <adcastel@uji.es>
2016-06-16 14:20:22 +0000

    checks added to glt_future_promise.c file

--- src/argobots/glt_future_promise.c
+++ src/argobots/glt_future_promise.c
@@ -23,19 +23,19 @@ GLT_func_prefix int glt_can_future_functions() {
 #ifndef CORE
 
 GLT_func_prefix void glt_future_create(int nbytes, GLT_future *newfuture) {
-    ABT_eventual_create(nbytes, newfuture);
+    CHECK(ABT_eventual_create(nbytes, newfuture),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_future_free(GLT_future *future) {
-    ABT_eventual_free(future);
+    CHECK(ABT_eventual_free(future),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_future_wait(GLT_future future, void **value) {
-    ABT_eventual_wait(future, value);
+    CHECK(ABT_eventual_wait(future, value),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_future_set(GLT_future future, void *value, int nbytes) {
-    ABT_eventual_set(future, value, nbytes);
+    CHECK(ABT_eventual_set(future, value, nbytes),ABT_SUCCESS);
 }
 #endif
 
@@ -51,22 +51,22 @@ GLT_func_prefix int glt_can_promise_functions() {
 #ifndef CORE
 
 GLT_func_prefix void glt_promise_create(uint32_t compartments, void(*cb_func)(void **arg), GLT_promise *newpromise) {
-    ABT_future_create(compartments, cb_func, newpromise);
+    CHECK(ABT_future_create(compartments, cb_func, newpromise),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_promise_free(GLT_promise *promise) {
-    ABT_future_free(promise);
+    CHECK(ABT_future_free(promise),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_promise_wait(GLT_promise promise) {
-    ABT_future_wait(promise);
+    CHECK(ABT_future_wait(promise),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_promise_test(GLT_promise promise, GLT_bool *flag) {
-    ABT_future_test(promise, flag);
+    CHECK(ABT_future_test(promise, flag),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_promise_set(GLT_promise promise, void * value) {
-    ABT_future_set(promise, value);
+    CHECK(ABT_future_set(promise, value),ABT_SUCCESS);
 }
 #endif
