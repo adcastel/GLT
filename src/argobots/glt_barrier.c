953337c7f511c3e10d9701d614072443fc63da2f
Adrian Castello <adcastel@uji.es>
2016-06-16 14:20:20 +0000

    checks added to glt_barrier.c file

--- src/argobots/glt_barrier.c
+++ src/argobots/glt_barrier.c
@@ -11,13 +11,13 @@
 #endif
 
 GLT_func_prefix void glt_barrier_create(int num_waiters, GLT_barrier *barrier) {
-    ABT_barrier_create(num_waiters, barrier);
+    CHECK(ABT_barrier_create(num_waiters, barrier),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_barrier_free(GLT_barrier *barrier) {
-    ABT_barrier_free(barrier);
+    CHECK(ABT_barrier_free(barrier),ABT_SUCCESS);
 }
 
 GLT_func_prefix void glt_barrier_wait(GLT_barrier *barrier) {
-    ABT_barrier_wait(*barrier);
+    CHECK(ABT_barrier_wait(*barrier),ABT_SUCCESS);
 }
