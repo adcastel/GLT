9164e0719bdf81edd83229e895aa9596f372af49
Adrian Castello <adcastel@uji.es>
2016-06-23 14:19:21 +0000

    tls moved to core and changed  file

---
+++ src/qthreads/glt_tls.c
@@ -0,0 +1,38 @@
+/* See COPYRIGHT in top-level directory. */
+/*
+ * TLS functions file.
+ */
+
+#ifndef FASTGLT
+#include <glt.h>
+#define GLT_func_prefix 
+#else
+#define GLT_func_prefix static inline
+#endif
+
+GLT_func_prefix int glt_can_tls_functions() {
+#ifdef CORE
+    return 0;
+#else
+    return 1;
+#endif
+}
+
+#ifndef CORE
+
+GLT_func_prefix void glt_key_create(void(*destructor)(void *value), GLT_key *newkey) {
+    myth_key_create(newkey, destructor);
+}
+
+GLT_func_prefix void glt_key_free(GLT_key *key) {
+    myth_key_delete(*key);
+}
+
+GLT_func_prefix void glt_key_set(GLT_key key, void *value) {
+    myth_setspecific(key, value);
+}
+
+GLT_func_prefix void glt_key_get(GLT_key key, void **value) {
+    *value = myth_getspecific(key);
+}
+#endif
