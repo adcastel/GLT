d6d2243d7999413c69a032252241108e16cb40fb
Adrian Castello <adcastel@uji.es>
2016-06-23 14:19:22 +0000

    tls moved to core and changed  file

--- src/argobots/glt_tls.c
+++ src/argobots/glt_tls.c
@@ -12,15 +12,7 @@
 #endif
 
 //tls functions
-GLT_func_prefix int glt_can_tls_functions() {
-#ifdef CORE
-    return 0;
-#else
-    return 1;
-#endif
-}
 
-#ifndef CORE
 
 GLT_func_prefix void glt_key_create(void(*destructor)(void *value), GLT_key *newkey) {
     CHECK(ABT_key_create(destructor, newkey),ABT_SUCCESS);
@@ -37,4 +29,3 @@ GLT_func_prefix void glt_key_set(GLT_key key, void *value) {
 GLT_func_prefix void glt_key_get(GLT_key key, void **value) {
     CHECK(ABT_key_get(key, value),ABT_SUCCESS);
 }
-#endif
