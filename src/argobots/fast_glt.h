35a32182dedd3057c546f3ed8c7d5a341c956afb
Adrian Castello <adcastel@uji.es>
2016-06-23 14:19:23 +0000

    tls moved to core and changed  file

--- src/argobots/fast_glt.h
+++ src/argobots/fast_glt.h
@@ -18,11 +18,11 @@
 #include <glt_timer.c>
 #include <glt_util.c>
 #include <glt_scheduler.c>
+#include <glt_tls.c>
 
 //Extended functions
 #include <glt_event.c>
 #include <glt_future_promise.c>
-#include <glt_tls.c>
 #include <glt_pool.c>
 #include<glt_self.c>
 #include<glt_thread.c>
